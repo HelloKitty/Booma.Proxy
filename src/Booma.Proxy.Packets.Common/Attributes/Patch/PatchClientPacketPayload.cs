4d1ef6f0a2d3f86288f32599e6cc3486837f40af
HelloKitty <andrew.blakely@ymail.com>
2017-09-25 19:35:09 -0500

    Added patch/ship link attributes for payloads

---
+++ src/Booma.Proxy.Packets.Common/Attributes/Patch/PatchClientPacketPayload.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Metadata attributes that implement <see cref="WireDataContractBaseLinkAttribute"/> making
+	/// it easier to safely link child payloads to their propert base type and
+	/// associate with in a typesafe fashion with their network operationcode enumeration value.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+	public sealed class PatchClientPacketPayload : WireDataContractBaseLinkAttribute
+	{
+		public PatchClientPacketPayload(int index) 
+			: base(index, typeof(PatchClientPacketPayload))
+		{
+
+		}
+	}
+}
