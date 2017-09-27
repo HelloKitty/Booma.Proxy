336541310fc8661187b56ac75b538d91f60a30f4
Soleil Rojas <soleilrojas@outlook.com>
2017-09-27 00:02:38 -0600

    Added File Information Done Ack

---
+++ src/Booma.Proxy.Packets.PatchServer/Payloads/Client/PatchingFileInformationDoneAckPayload.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy.Packets.PatchServer.Payloads.Client
+{
+	/// <summary>
+	/// Client file information report
+	/// The server decides to patch a file based on this data
+	/// </summary>
+	[WireDataContract]
+	[PatchServerPacketPayload(PatchNetworkOperationCodes.PATCH_FILE_LIST_DONE)]
+	public sealed class PatchingFileInformationDoneAckPayload : PSOBBPatchPacketPayloadClient
+	{
+		public PatchingFileInformationDoneAckPayload()
+		{
+		}
+	}
+}
