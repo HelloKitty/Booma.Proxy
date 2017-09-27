0babbc2ab2ccd7cf7d4b08afc40dd0c5c3a929a0
HelloKitty <andrew.blakely@ymail.com>
2017-09-26 20:04:29 -0500

    Refactored Packets; Added some more test coverage

---
+++ src/Booma.Proxy.Packets.Common/Message/PSOBBNetworkOutgoingMessage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Full message for a PSOBB 
+	/// </summary>
+	[WireDataContract]
+	public sealed class PSOBBNetworkOutgoingMessage
+	{
+		/// <summary>
+		/// The header for the outgoing message.
+		/// </summary>
+		[WireMember(1)]
+		public PSOBBPacketHeader Header { get; }
+
+		/// <summary>
+		/// The byte representation of the payload.
+		/// </summary>
+		[ReadToEnd]
+		[WireMember(2)]
+		public byte[] Payload { get; }
+
+		/// <summary>
+		/// Creates a new outgoing message envelope. Represents a complete
+		/// logical packet including the header and the payload byte representation.
+		/// </summary>
+		/// <param name="payload">The payload.</param>
+		public PSOBBNetworkOutgoingMessage([NotNull] byte[] payload)
+		{
+			if(payload == null) throw new ArgumentNullException(nameof(payload));
+
+			Payload = payload;
+			Header = new PSOBBPacketHeader(payload.Length + 2);
+		}
+
+		//Serializer ctor
+		protected PSOBBNetworkOutgoingMessage()
+		{
+			
+		}
+	}
+}
---
+++ src/Booma.Proxy.Packets.PatchServer/PacketPatchServerMetadataMarker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Just a simple referenceable Type for reflection purposes.
+	/// </summary>
+	public static class PacketPatchServerMetadataMarker
+	{
+		public static IEnumerable<Type> SerializableTypes { get; } = typeof(PacketPatchServerMetadataMarker)
+			.Assembly
+			.GetTypes()
+			.Where(t => t.GetCustomAttribute(typeof(WireDataContractBaseLinkAttribute)) != null)
+			.ToList();
+	}
+}
