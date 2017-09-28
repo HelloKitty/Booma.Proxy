8c0f058319f1cab2887f4cb0c28f17285772e401
HelloKitty <andrew.blakely@ymail.com>
2017-09-28 02:58:23 -0500

    Added ICryptoKeyInitializable

---
+++ src/Booma.Proxy.Crypto/Services/ICryptoKeyInitializable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Contract for types that can have IV/Keys
+	/// inserted into them.
+	/// </summary>
+	public interface ICryptoKeyInitializable<TKeyType>
+	{
+		/// <summary>
+		/// Sets the crypto service with the provided key
+		/// </summary>
+		/// <param name="key">The key to set.</param>
+		void SetKey(TKeyType key);
+	}
+}
