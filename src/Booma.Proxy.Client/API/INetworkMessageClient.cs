b0127cda2a942373d6751d808a72d7e04e037cb4
HelloKitty <andrew.blakely@ymail.com>
2017-09-28 04:54:34 -0500

    Addec consolidated netclient interface

---
+++ src/Booma.Proxy.Client/API/INetworkMessageClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Consolidated interface for client that implements reading and writing of the specified generic payloads.
+	/// </summary>
+	/// <typeparam name="TReadPayloadType"></typeparam>
+	/// <typeparam name="TWritePayloadType"></typeparam>
+	public interface INetworkMessageClient<TReadPayloadType, TWritePayloadType> : 
+		IPacketPayloadReadable<TReadPayloadType>, IPacketPayloadWritable<TWritePayloadType>, IConnectable
+		where TReadPayloadType : class
+		where TWritePayloadType : class
+	{
+
+	}
+}
