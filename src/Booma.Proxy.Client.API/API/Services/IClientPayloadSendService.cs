039ab7dc76c3561c43a9335c10278dfd9cd745f3
HelloKitty <andrew.blakely@ymail.com>
2017-09-28 18:32:29 -0500

    Added IClientPayloadSendService

---
+++ src/Booma.Proxy.Client.API/API/Services/IClientPayloadSendService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Contract for services that provide the ability to send payloads.
+	/// </summary>
+	/// <typeparam name="TPayloadBaseType"></typeparam>
+	public interface IClientPayloadSendService<TPayloadBaseType>
+		where TPayloadBaseType : class
+	{
+		//TODO: We will implement a priority or immediate sending overloads in the future
+
+		/// <summary>
+		/// Sends the provided <see cref="payload"/>
+		/// </summary>
+		/// <typeparam name="TPayloadType"></typeparam>
+		/// <param name="payload"></param>
+		/// <returns></returns>
+		Task<SendResult> SendMessage<TPayloadType>(TPayloadType payload)
+			where TPayloadType : class, TPayloadBaseType;
+	}
+}
