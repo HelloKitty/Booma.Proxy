c82e49212221133a6a77e330fc0a955a2b1732ad
HelloKitty <andrew.blakely@ymail.com>
2017-09-28 20:22:26 -0500

    Added INetworkMessageProducer interface

---
+++ src/Booma.Proxy.Client.API/API/Message/INetworkMessageProducer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Contract for types that can produce network messages.
+	/// </summary>
+	/// <typeparam name="TPayloadBaseType">The network message type.</typeparam>
+	public interface INetworkMessageProducer<TPayloadBaseType>
+		where TPayloadBaseType : class
+	{
+		/// <summary>
+		/// Produces a <see cref="PSOBBNetworkIncomingMessage{TPayloadType}"/> asyncronously.
+		/// The task will complete when a network message is available.
+		/// </summary>
+		/// <returns>Returns a future that will complete when a message is available.</returns>
+		Task<PSOBBNetworkIncomingMessage<TPayloadBaseType>> ProduceAsync();
+
+		//TODO: Do we need a syncronous version?
+	}
+}
