c81021e38726b5547a215e1463ad5b361b6a9355
HelloKitty <andrew.blakely@ymail.com>
2017-09-28 19:07:47 -0500

    Added IClientMessageContextFactory

---
+++ src/Booma.Proxy.Client.API/API/Message/IClientMessageContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Contract for types that provide construction functionality
+	/// for <see cref="IClientMessageContext{TPayloadBaseType}"/>.
+	/// </summary>
+	public interface IClientMessageContextFactory
+	{
+		/// <summary>
+		/// Creates a new client message context based around the <see cref="client"/>
+		/// and contextless relative to the actual message.
+		/// </summary>
+		/// <typeparam name="TClientType">The client type.</typeparam>
+		/// <typeparam name="TPayloadBaseType">The payload basetype.</typeparam>
+		/// <param name="client">The client to use as the base for the context/</param>
+		/// <returns>A new message context.</returns>
+		IClientMessageContext<TPayloadBaseType> Create<TClientType, TPayloadBaseType>(TClientType client)
+			where TPayloadBaseType : class
+			where TClientType : IConnectable, IDisconnectable, IPacketPayloadWritable<TPayloadBaseType>;
+	}
+}
