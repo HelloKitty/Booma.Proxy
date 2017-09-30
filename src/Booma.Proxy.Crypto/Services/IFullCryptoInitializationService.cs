7acc86466a4a1a9a435d5911d80a43f613ac4e73
HelloKitty <andrew.blakely@ymail.com>
2017-09-30 01:56:34 -0500

    Added consolidated key initialization

---
+++ src/Booma.Proxy.Crypto/Services/IFullCryptoInitializationService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// A pair of crypto initializers.
+	/// </summary>
+	/// <typeparam name="TVectorType">The type of vector input.</typeparam>
+	public interface IFullCryptoInitializationService<TVectorType>
+	{
+		/// <summary>
+		/// The encryption initializable.
+		/// </summary>
+		ICryptoKeyInitializable<TVectorType> EncryptionInitializable { get; }
+
+		/// <summary>
+		/// The decryption initializable.
+		/// </summary>
+		ICryptoKeyInitializable<TVectorType> DecryptionInitializable { get; }
+	}
+}
