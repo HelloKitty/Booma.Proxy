2c5831712fb63dc5777ca52d1ee68f39731dad93
HelloKitty <andrew.blakely@ymail.com>
2017-09-30 20:28:52 -0500

    0x03 BB_WELCOME_TYPE implemented

---
+++ src/Booma.Proxy.Packets.LoginServer/Payloads/Server/LoginWelcomePayload.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	/*typedef struct bb_welcome
+	{
+		bb_pkt_hdr_t hdr;
+		char copyright[0x60];
+		uint8_t svect[48];
+		uint8_t cvect[48];
+	}
+	PACKED bb_welcome_pkt;*/
+
+	/// <summary>
+	/// Login server payload responsible for providing the encryption IVs for the
+	/// blowfish encryption.
+	/// </summary>
+	[WireDataContract]
+	[LoginServerPacketPayload(LoginNetworkOperationCode.BB_WELCOME_TYPE)]
+	public sealed class LoginWelcomePayload : PSOBBLoginPacketPayloadServer
+	{
+		/// <summary>
+		/// Copyright message that the server sends to
+		/// the client which it verifies.
+		/// </summary>
+		[KnownSize(60)]
+		[WireMember(1)]
+		public string CopyrightMessage { get; }
+
+		/// <summary>
+		/// Encryption initialization vector
+		/// for the login server crypto.
+		/// </summary>
+		[KnownSize(48)]
+		[WireMember(2)]
+		public byte[] ServerVector { get; }
+
+		/// <summary>
+		/// Decryption initialization vector
+		/// for the login server crypto.
+		/// </summary>
+		[KnownSize(48)]
+		[WireMember(2)]
+		public byte[] ClientVector { get; }
+
+		//Serializer ctor
+		private LoginWelcomePayload()
+		{
+			
+		}
+	}
+}
