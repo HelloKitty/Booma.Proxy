09e1846eaf263ebac0d6b46b1ffc40f903b06a2a
HelloKitty <andrew.blakely@ymail.com>
2017-10-07 04:50:23 -0500

    Added LoginWelcomePayloadHandler to Unity.Login

---
+++ src/Booma.Proxy.Client.Unity.Login/Handlers/LoginMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Simplified type of <see cref="BaseUnityMessageHandler{TIncomingPayloadBaseType,TOutgoingPayloadType,TPayloadType}"/>
+	/// for login message handlers.
+	/// </summary>
+	/// <typeparam name="TPayloadType"></typeparam>
+	public abstract class LoginMessageHandler<TPayloadType> : BaseUnityMessageHandler<PSOBBLoginPacketPayloadServer, PSOBBLoginPacketPayloadClient, TPayloadType> 
+		where TPayloadType : PSOBBLoginPacketPayloadServer
+	{
+
+	}
+}
---
+++ src/Booma.Proxy.Client.Unity.Login/Handlers/LoginWelcomePayloadHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SceneJect.Common;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Handler for the <see cref="LoginWelcomePayload"/>.
+	/// </summary>
+	[Injectee]
+	public sealed class LoginWelcomePayloadHandler : LoginMessageHandler<LoginWelcomePayload>
+	{
+		/// <summary>
+		/// Crypto initialization service that can be init from the welcome message.
+		/// </summary>
+		[Inject]
+		private IFullCryptoInitializationService<byte[]> CryptoInitializer { get; }
+
+		/// <inheritdoc />
+		public override async Task HandleMessage(IClientMessageContext<PSOBBLoginPacketPayloadClient> context, LoginWelcomePayload payload)
+		{
+			//the crypto needs to init after the welcome message
+			CryptoInitializer.EncryptionInitializable.Initialize(payload.ClientVector);
+			CryptoInitializer.DecryptionInitializable.Initialize(payload.ServerVector);
+
+			//TODO: Abstract the username/pass and version string stuff out
+			//After the welcome message is recieved we need to send the login.
+			await context.PayloadSendService.SendMessage(new LoginLoginRequest93Payload(0x41, "glader", "playpso69", ClientVerificationData.FromVersionString("Destiny v0.6")));
+		}
+	}
+}
