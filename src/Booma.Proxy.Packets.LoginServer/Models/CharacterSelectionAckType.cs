eb7651c0632a97464fc2e581dbff2f9b96917f20
HelloKitty <andrew.blakely@ymail.com>
2017-10-09 09:21:37 -0500

    implemented E4 CharAck

---
+++ src/Booma.Proxy.Packets.LoginServer/Models/CharacterSelectionAckType.cs
@@ -0,0 +1,14 @@
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// https://github.com/Sylverant/login_server/blob/d275702120ade56ce0b8b826a6c549753587d7e1/src/packets.h#L1729
+	/// </summary>
+	public enum CharacterSelectionAckType : int
+	{
+		BB_CHAR_ACK_UPDATE = 0,
+
+		BB_CHAR_ACK_SELECT = 1,
+
+		BB_CHAR_ACK_NONEXISTANT = 2
+	}
+}
