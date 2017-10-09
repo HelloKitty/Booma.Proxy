a93c857ef7a21faf8bd2b6bde5b4e2a025f377c3
HelloKitty <andrew.blakely@ymail.com>
2017-10-09 09:55:27 -0500

    implement 02E8 checksum response

---
+++ src/Booma.Proxy.Packets.LoginServer/Models/LoginChecksumResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Booma.Proxy
+{
+	/// <summary>
+	/// Enumeration of checksum results.
+	/// </summary>
+	public enum LoginChecksumResult : uint
+	{
+		/// <summary>
+		/// Syl sends 1 for success.
+		/// </summary>
+		Success = 1,
+	}
+}
---
+++ src/Booma.Proxy.Packets.LoginServer/Payloads/Server/LoginChecksumResponsePayload.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Response to the <see cref="LoginChecksumRequestPayload"/>.
+	/// </summary>
+	[WireDataContract]
+	[LoginServerPacketPayload(LoginNetworkOperationCodes.BB_CHECKSUM_ACK_TYPE)]
+	public sealed class LoginChecksumResponsePayload : PSOBBLoginPacketPayloadServer, IResponseCodePayload<LoginChecksumResult>
+	{
+		/// <inheritdoc />
+		[WireMember(1)]
+		public LoginChecksumResult ResponseCode { get; }
+
+		//Serializer ctor
+		private LoginChecksumResponsePayload()
+		{
+			
+		}
+	}
+}
