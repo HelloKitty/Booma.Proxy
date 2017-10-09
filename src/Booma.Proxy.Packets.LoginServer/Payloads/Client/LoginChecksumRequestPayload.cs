9bffee2f3a37dc035e33da08e08260bfd133df11
HelloKitty <andrew.blakely@ymail.com>
2017-10-09 09:40:05 -0500

    implemented 1E8 checksum request

---
+++ src/Booma.Proxy.Packets.LoginServer/Payloads/Client/LoginChecksumRequestPayload.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Checksum message sent after character information.
+	/// </summary>
+	[WireDataContract]
+	[LoginClientPacketPayload(LoginNetworkOperationCodes.BB_CHECKSUM_TYPE)]
+	public sealed class LoginChecksumRequestPayload : PSOBBLoginPacketPayloadClient
+	{
+		//TODO: What is this a checksum of?
+		/// <summary>
+		/// The checksum.
+		/// </summary>
+		[WireMember(1)]
+		public uint Checksum { get; }
+
+		[WireMember(2)]
+		private uint Padding { get; }
+
+		public LoginChecksumRequestPayload(uint checksum)
+		{
+			Checksum = checksum;
+		}
+
+		//Serializer ctor
+		private LoginChecksumRequestPayload()
+		{
+			
+		}
+	}
+}
