bf60e1e48de977e24c3d9b91f8e9759135a96c75
HelloKitty <andrew.blakely@ymail.com>
2017-10-08 20:08:16 -0500

    implemented E0 LoginOptionsRequestPayload

---
+++ src/Booma.Proxy.Packets.LoginServer/Payloads/Client/LoginOptionsRequestPayload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Empty payload the client sends to get character specific options data
+	/// that are saved on the server.
+	/// </summary>
+	[WireDataContract]
+	[LoginClientPacketPayload(LoginNetworkOperationCodes.BB_OPTION_REQUEST_TYPE)]
+	public sealed class LoginOptionsRequestPayload : PSOBBLoginPacketPayloadClient
+	{
+		//Empty, just a payload that alerts the server to the desire of options
+
+		public LoginOptionsRequestPayload()
+		{
+			
+		}
+	}
+}
