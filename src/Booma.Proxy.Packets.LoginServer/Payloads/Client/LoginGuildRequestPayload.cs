3f4fbf330db46dbc94fca6044853dd5587812b3f
HelloKitty <andrew.blakely@ymail.com>
2017-10-09 10:14:27 -0500

    Added 03E8 guild info request

---
+++ src/Booma.Proxy.Packets.LoginServer/Payloads/Client/LoginGuildRequestPayload.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	//TODO: What is this packet for/about?
+	[WireDataContract]
+	[LoginClientPacketPayload(LoginNetworkOperationCodes.BB_GUILD_REQUEST_TYPE)]
+	public class LoginGuildRequestPayload : PSOBBLoginPacketPayloadClient
+	{
+		//Empty payload that requests guild stuff? Idk
+
+		public LoginGuildRequestPayload()
+		{
+			
+		}
+	}
+}
