307c579aa4c787364a8951864563adbbb0834b0a
HelloKitty <andrew.blakely@ymail.com>
2017-10-10 13:39:32 -0500

    implemented B1 Timestamp event

---
+++ src/Booma.Proxy.Packets.LoginServer/Payloads/Server/LoginTimestampEventPayload.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	/*typedef struct bb_timestamp {
+		bb_pkt_hdr_t hdr;
+		char timestamp[28];
+	} PACKED bb_timestamp_pkt;*/
+
+	//Syl: https://github.com/Sylverant/login_server/blob/d275702120ade56ce0b8b826a6c549753587d7e1/src/packets.h#L349
+	/// <summary>
+	/// Payload event send by the server that contains
+	/// a timestamp string.
+	/// </summary>
+	[WireDataContract]
+	[LoginServerPacketPayload(LoginNetworkOperationCodes.TIMESTAMP_TYPE)]
+	public sealed class LoginTimestampEventPayload : PSOBBLoginPacketPayloadServer
+	{
+		/// <summary>
+		/// The time stamp.
+		/// See Syl for format.
+		/// </summary>
+		[KnownSize(28)]
+		[WireMember(1)]
+		public string Timestamp { get; }
+
+		//Serializer ctor
+		private LoginTimestampEventPayload()
+		{
+			
+		}
+	}
+}
