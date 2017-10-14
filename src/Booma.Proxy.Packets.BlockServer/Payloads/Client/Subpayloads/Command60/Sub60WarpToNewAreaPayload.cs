df7c10993a82f5a3fa12e7e4424109b58f7113bd
HelloKitty <andrew.blakely@ymail.com>
2017-10-14 12:26:30 -0500

    Implemented 0x1F subcommand60 WarpTo

---
+++ src/Booma.Proxy.Packets.BlockServer/Payloads/Client/Subpayloads/Command60/Sub60WarpToNewAreaPayload.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	//Syl: https://github.com/Sylverant/ship_server/blob/master/src/subcmd.h#L412
+	[WireDataContract]
+	[SubCommand60Client(SubCommand60OperationCode.WrapToNewArea)]
+	public sealed class Sub60WarpToNewAreaPayload : BlockNetworkCommandEventClientPayload
+	{
+		/// <summary>
+		/// The ID associated with this client.
+		/// </summary>
+		[WireMember(1)]
+		public short ClientId { get; }
+
+		/// <summary>
+		/// The Map/Level/Zone to warp to.
+		/// (Ex. go from Forest 1 to Forest 2)
+		/// </summary>
+		[WireMember(2)]
+		public int Zone { get; }
+
+		/// <inheritdoc />
+		public Sub60WarpToNewAreaPayload(short clientId, int zone)
+		{
+			if(zone < 0) throw new ArgumentOutOfRangeException(nameof(zone));
+			if(clientId < 0) throw new ArgumentOutOfRangeException(nameof(clientId));
+
+			ClientId = clientId;
+			Zone = zone;
+		}
+
+		private Sub60WarpToNewAreaPayload()
+		{
+			
+		}
+	}
+}
