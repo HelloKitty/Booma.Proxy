312b87b05f24b99df186f295b7ab63c9cad9bf20
HelloKitty <andrew.blakely@ymail.com>
2017-10-14 12:42:41 -0500

    Implemented 0x23 subcommand60 FinishedWarp

---
+++ src/Booma.Proxy.Packets.BlockServer/Payloads/Client/Subpayloads/Command60/Sub60FinishedWarpingBurstingPayload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Payload that tells the server we've finsihed loading/bursting/warping.
+	/// We should have sent a warp request to the server before this so it should know where we
+	/// were going.
+	/// </summary>
+	[WireDataContract]
+	[SubCommand60Client(SubCommand60OperationCode.EnterFreshlyWrappedZoneCommand)]
+	public sealed class Sub60FinishedWarpingBurstingPayload : BlockNetworkCommandEventClientPayload
+	{
+		//Packet is empty. Just tells the server we bursted/warped finished.
+
+		public Sub60FinishedWarpingBurstingPayload()
+		{
+			
+		}
+	}
+}
