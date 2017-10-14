ae0cc4ec927a5048f842d9aceac46b97fe4552f5
HelloKitty <andrew.blakely@ymail.com>
2017-10-14 13:23:34 -0500

    Implemented 0x6F subcommand60 WarpStartEvent

---
+++ src/Booma.Proxy.Packets.BlockServer/Payloads/Server/Subpayloads/Sub60BeginWarpEventPayload.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	//Tethella: https://github.com/justnoxx/psobb-tethealla/blob/master/ship_server/ship_server.c#L2720
+	/// <summary>
+	/// Sent before a client begins to load and warp to a new
+	/// area/map/level.
+	/// </summary>
+	[WireDataContract]
+	[SubCommand60Server(SubCommand60OperationCode.BurstType5)]
+	public sealed class Sub60BeginWarpEventPayload : BlockNetworkCommandEventServerPayload
+	{
+		//TODO: This are about 518 bytes here for quest data
+		[ReadToEnd]
+		[WireMember(1)]
+		private byte[] QuestData { get; } = new byte[0];
+
+		//Serializer ctor
+		private Sub60BeginWarpEventPayload()
+		{
+			
+		}
+	}
+}
