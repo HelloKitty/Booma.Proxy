ae5a3251b803ae594df885fc5abba15b1fcf342c
HelloKitty <andrew.blakely@ymail.com>
2017-10-14 02:06:21 -0500

    Updated build script/process

--- build.bat
+++ build.bat
@@ -1,10 +1,8 @@
 nuget.exe restore Booma.Proxy.sln
 dotnet restore Booma.Proxy.sln
-dotnet publish src/Booma.Proxy.Client.Unity.Login/Booma.Proxy.Client.Unity.Login.csproj -c release
-dotnet publish src/Booma.Proxy.Client.Unity.Editor/Booma.Proxy.Client.Unity.Editor.csproj -c release
+dotnet publish src/Booma.Proxy.Client.Unity.BuildAll/Booma.Proxy.Client.Unity.BuildAll.csproj -c release
 
 if not exist "build\client\release" mkdir build\client\release
-xcopy src\Booma.Proxy.Client.Unity.Editor\bin\Release\net46\publish build\client\release /Y /q /EXCLUDE:BuildExclude.txt
-xcopy src\Booma.Proxy.Client.Unity.Login\bin\Release\net46\publish build\client\release /Y /q /EXCLUDE:BuildExclude.txt
+xcopy src\Booma.Proxy.Client.Unity.BuildAll\bin\Release\net46\publish build\client\release /Y /q /EXCLUDE:BuildExclude.txt
 
 PAUSE
