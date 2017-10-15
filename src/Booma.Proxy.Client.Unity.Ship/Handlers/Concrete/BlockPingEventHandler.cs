a721553ed3220a2c8d09122069f91d148375e38e
HelloKitty <andrew.blakely@ymail.com>
2017-10-15 05:44:57 -0500

    Added blockpingeventhandler for 1D

---
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Concrete/BlockPingEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// The ping event handler. Will just send the ping response.
+	/// </summary>
+	public class BlockPingEventHandler : GameMessageHandler<BlockClientPingEventPayload>
+	{
+		/// <inheritdoc />
+		public override Task HandleMessage(IClientMessageContext<PSOBBGamePacketPayloadClient> context, BlockClientPingEventPayload payload)
+		{
+			//Just send the ping response; otherwise we'll be disconnected.
+			context.PayloadSendService.SendMessage(new BlockClientPingResponsePayload());
+
+			return Task.CompletedTask;
+		}
+	}
+}
