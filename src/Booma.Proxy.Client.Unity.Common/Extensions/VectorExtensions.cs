849876fd8ad6327d0d8acb03937bf99b05ec557e
HelloKitty <andrew.blakely@ymail.com>
2017-10-16 07:56:22 -0500

    Added Vector3 to Vector3 extension

--- src/Booma.Proxy.Client.Unity.Common/Extensions/VectorExtensions.cs
+++ src/Booma.Proxy.Client.Unity.Common/Extensions/VectorExtensions.cs
@@ -21,6 +21,17 @@ namespace Booma.Proxy
 		}
 
 		/// <summary>
+		/// Creates a new <see cref="Vector3"/> with the original <see cref="Vector3{T}"/>'s
+		/// X, Y and Z components mapped to the new <see cref="Vector3"/>'s X, Y and Z.
+		/// </summary>
+		/// <param name="vector">The vector to convert.</param>
+		/// <returns>A Unity3D <see cref="Vector3"/> represetnation of the <see cref="Vector3{T}"/>.</returns>
+		public static Vector3 ToUnityVector3(this Vector3<float> vector)
+		{
+			return new Vector3(vector.X, vector.Y, vector.Z);
+		}
+
+		/// <summary>
 		/// Creates a new <see cref="Vector3"/> with the original <see cref="Vector2{T}"/>'s
 		/// X and Y components mapped to the new <see cref="Vector3"/>'s X and Z.
 		/// With an optional Y value parameter to initialize to the Y.
