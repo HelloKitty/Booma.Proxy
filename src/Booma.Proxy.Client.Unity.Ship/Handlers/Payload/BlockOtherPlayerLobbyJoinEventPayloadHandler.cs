1cff23e018a7e5737102110a5713eac31410c7a0
HelloKitty <andrew.blakely@ymail.com>
2017-10-16 07:23:55 -0500

    Added Lobby join event logging

---
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Payload/BlockOtherPlayerLobbyJoinEventPayloadHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public sealed class BlockOtherPlayerLobbyJoinEventPayloadHandler : GameMessageHandler<BlockOtherPlayerJoinedLobbyEventPayload>
+	{
+		/// <inheritdoc />
+		public override Task HandleMessage(IClientMessageContext<PSOBBGamePacketPayloadClient> context, BlockOtherPlayerJoinedLobbyEventPayload payload)
+		{
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"Player Lobby Join: {payload.ClientId} LeaderId: {payload.LeaderId} EventId: {payload.EventId} Lobby: {payload.LobbyNumber}");
+
+			return Task.CompletedTask;
+		}
+	}
+}
