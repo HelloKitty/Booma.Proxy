739ce69151491b2cd46d550e24321963a365e5ce
HelloKitty <andrew.blakely@ymail.com>
2017-10-16 08:31:52 -0500

    Added ToUnityVector2 extension

--- src/Booma.Proxy.Client.Unity.Common/Extensions/VectorExtensions.cs
+++ src/Booma.Proxy.Client.Unity.Common/Extensions/VectorExtensions.cs
@@ -21,6 +21,17 @@ namespace Booma.Proxy
 		}
 
 		/// <summary>
+		/// Creates a new <see cref="Vector2"/> with the original <see cref="Vector2{T}"/>'s
+		/// X and Y components mapped to the new <see cref="Vector2"/>'s X and Y.
+		/// </summary>
+		/// <param name="vector">The vector to convert.</param>
+		/// <returns>A Unity3D <see cref="Vector2"/> represetnation of the <see cref="Vector2{T}"/>.</returns>
+		public static Vector2 ToUnityVector2(this Vector2<float> vector)
+		{
+			return new Vector2(vector.X, vector.Y);
+		}
+
+		/// <summary>
 		/// Creates a new <see cref="Vector3"/> with the original <see cref="Vector3{T}"/>'s
 		/// X, Y and Z components mapped to the new <see cref="Vector3"/>'s X, Y and Z.
 		/// </summary>
