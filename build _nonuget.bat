af91f760ed5db3926b633f6e13eb5fefdd454c57
HelloKitty <andrew.blakely@ymail.com>
2017-10-16 04:46:25 -0500

    Added a no nuget buildscript

---
+++ build _nonuget.bat
@@ -0,0 +1,6 @@
+dotnet publish src/Booma.Proxy.Client.Unity.BuildAll/Booma.Proxy.Client.Unity.BuildAll.csproj -c release
+
+if not exist "build\client\release" mkdir build\client\release
+xcopy src\Booma.Proxy.Client.Unity.BuildAll\bin\Release\net46\publish build\client\release /Y /q /EXCLUDE:BuildExclude.txt
+
+PAUSE
