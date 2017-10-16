f8823b668e000b1ceecd8ebf4dcd2a799a0d3346
HelloKitty <andrew.blakely@ymail.com>
2017-10-16 07:24:37 -0500

    Added misc extensions for Handler/Vector

---
+++ src/Booma.Proxy.Client.Unity.Common/Extensions/MessageHandlerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public static class MessageHandlerExtensions
+	{
+		/// <summary>
+		/// Gets the loggable message name that the handler handles.
+		/// </summary>
+		/// <typeparam name="TSubMessageType">The message type.</typeparam>
+		/// <typeparam name="TPayloadType">The payload type.</typeparam>
+		/// <param name="handler">The message handler.</param>
+		/// <returns>The string name of the message type.</returns>
+		public static string MessageName<TSubMessageType, TPayloadType>(this SubMessageMessageHandler<TSubMessageType, TPayloadType> handler) 
+			where TPayloadType : PSOBBGamePacketPayloadServer
+		{
+			if(handler == null) throw new ArgumentNullException(nameof(handler));
+
+			return typeof(TSubMessageType).Name;
+		}
+	}
+}
---
+++ src/Booma.Proxy.Client.Unity.Common/Extensions/VectorExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public static class VectorExtensions
+	{
+		/// <summary>
+		/// Creates a new <see cref="Vector3"/> with the original <see cref="Vector2{T}"/>'s
+		/// X and Y components mapped to the new <see cref="Vector3"/>'s X and Y.
+		/// </summary>
+		/// <param name="vector">The vector to convert.</param>
+		/// <returns>A Unity3D <see cref="Vector3"/> represetnation of the <see cref="Vector2{T}"/>.</returns>
+		public static Vector3 ToUnityVector3(this Vector2<float> vector)
+		{
+			return new Vector3(vector.X, vector.Y);
+		}
+
+		/// <summary>
+		/// Creates a new <see cref="Vector3"/> with the original <see cref="Vector2{T}"/>'s
+		/// X and Y components mapped to the new <see cref="Vector3"/>'s X and Z.
+		/// With an optional Y value parameter to initialize to the Y.
+		/// </summary>
+		/// <param name="vector">The vector to convert.</param>
+		/// <param name="optionalY">Optional Y value to initialize for the Y component of the Unity vector.</param>
+		/// <returns>A Unity3D <see cref="Vector3"/> represetnation of the <see cref="Vector2{T}"/>.</returns>
+		public static Vector3 ToUnityVector3XZ(this Vector2<float> vector, float optionalY = 0.0f)
+		{
+			return new Vector3(vector.X, optionalY, vector.Y);
+		}
+	}
+}
