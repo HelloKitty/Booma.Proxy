8ff35515b0f7a5528178a13f52959e2d4e5d3ec5
HelloKitty <andrew.blakely@ymail.com>
2017-10-16 07:24:16 -0500

    Added teleport command event logging

---
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Command/Movement/BlockTeleportToPositionCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public sealed class BlockTeleportToPositionCommandHandler : Command60Handler<Sub60TeleportToPositionCommand>
+	{
+		/// <inheritdoc />
+		protected override Task HandleSubMessage(IClientMessageContext<PSOBBGamePacketPayloadClient> context, Sub60TeleportToPositionCommand command)
+		{
+			//TODO: Don't do anything with this
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"Recieved {nameof(Sub60TeleportToPositionCommand)} ClientId: {command.ClientId} X: {command.Position.X} Y: {command.Position.Y} Z: {command.Position.Z}");
+
+			return Task.CompletedTask;
+		}
+	}
+}
