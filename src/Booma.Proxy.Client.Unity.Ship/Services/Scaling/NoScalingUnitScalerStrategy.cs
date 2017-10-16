1d83cf4fbf812c9135a10052eb23ce62f6441657
HelloKitty <andrew.blakely@ymail.com>
2017-10-16 04:13:15 -0500

    Implemented scaler strategies

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Scaling/NoScalingUnitScalerStrategy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Strategy that doesn't scale or unscale the provided components at all.
+	/// </summary>
+	public sealed class NoScalingUnitScalerStrategy : IUnitScalerStrategy
+	{
+		/// <inheritdoc />
+		public float ScaleX(float x)
+		{
+			return x;
+		}
+
+		/// <inheritdoc />
+		public float ScaleY(float y)
+		{
+			return y;
+		}
+
+		/// <inheritdoc />
+		public float ScaleZ(float z)
+		{
+			return z;
+		}
+
+		/// <inheritdoc />
+		public float UnScaleX(float x)
+		{
+			return x;
+		}
+
+		/// <inheritdoc />
+		public float UnScaleY(float y)
+		{
+			return y;
+		}
+
+		/// <inheritdoc />
+		public float UnScaleZ(float z)
+		{
+			return z;
+		}
+	}
+}
