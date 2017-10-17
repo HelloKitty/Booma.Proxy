fe394f3037dfcb99f6e468ae670f9986653d30fe
HelloKitty <andrew.blakely@ymail.com>
2017-10-17 07:39:38 -0500

    Added invalid player context object

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Message/Context/Concrete/InvalidPlayerCommandMessageContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public class InvalidPlayerCommandMessageContext : INetworkPlayerFullCommandMessageContext
+	{
+		public static InvalidPlayerCommandMessageContext Instance { get; } = new InvalidPlayerCommandMessageContext();
+
+		/// <inheritdoc />
+		public bool isValid { get; } = false;
+
+		/// <inheritdoc />
+		public INetworkPlayer RemotePlayer { get; }
+
+		/// <inheritdoc />
+		public INetworkPlayer LocalPlayer { get; }
+	}
+}
