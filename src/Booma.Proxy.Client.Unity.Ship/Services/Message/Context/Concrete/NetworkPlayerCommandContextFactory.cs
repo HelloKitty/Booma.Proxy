5d754b0e106104d2a9c7bf9ccced9dee408be8ba
HelloKitty <andrew.blakely@ymail.com>
2017-10-16 21:57:53 -0500

    Implemented network player command context/factory

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Message/Context/Concrete/NetworkPlayerCommandContextFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+
+	/// <summary>
+	/// Context factory that can build both
+	/// <see cref="INetworkPlayerCommandMessageContext"/> and <see cref="INetworkPlayerFullCommandMessageContext"/>s.
+	/// </summary>
+	public sealed class NetworkPlayerCommandContextFactory : ICommandMessageContextFactory<ICommandClientIdentifiable, INetworkPlayerFullCommandMessageContext>
+	{
+		/// <summary>
+		/// The network player collection.
+		/// </summary>
+		private INetworkPlayerCollection PlayerCollection { get; }
+
+		public NetworkPlayerCommandContextFactory(INetworkPlayerCollection playerCollection)
+		{
+			if(playerCollection == null) throw new ArgumentNullException(nameof(playerCollection));
+
+			PlayerCollection = playerCollection;
+		}
+
+		/// <inheritdoc />
+		public INetworkPlayerFullCommandMessageContext Create(ICommandClientIdentifiable message)
+		{
+			//TODO: Create default contexts
+			if(!PlayerCollection.ContainsId(message.ClientId))
+				return null;
+
+			//Just create the new context.
+			return new NetworkPlayerCommandMessageContext(PlayerCollection[message.ClientId], PlayerCollection.Local);
+		}
+	}
+}
---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Message/Context/Concrete/NetworkPlayerCommandMessageContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public sealed class NetworkPlayerCommandMessageContext : INetworkPlayerFullCommandMessageContext
+	{
+		/// <inheritdoc />
+		public bool isValid { get; } = true;
+
+		/// <inheritdoc />
+		public INetworkPlayer Remote { get; }
+
+		/// <inheritdoc />
+		public INetworkPlayer Local { get; }
+
+		/// <inheritdoc />
+		public NetworkPlayerCommandMessageContext(INetworkPlayer remote, INetworkPlayer local)
+		{
+			if(remote == null) throw new ArgumentNullException(nameof(remote));
+			if(local == null) throw new ArgumentNullException(nameof(local));
+
+			Remote = remote;
+			Local = local;
+		}
+	}
+}
