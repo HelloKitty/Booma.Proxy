c8de222eedab4d0a0ccdd07b639e1ab810106a6b
HelloKitty <andrew.blakely@ymail.com>
2017-10-17 00:48:44 -0500

    Implemented Identity API

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/Identity/EntityType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	//TODO: Figure out how we should implement this
+	/// <summary>
+	/// Enumeration of all entity types.
+	/// </summary>
+	public enum EntityType
+	{
+		/// <summary>
+		/// Represents a typeless entity.
+		/// </summary>
+		None = 0,
+
+		/// <summary>
+		/// Represents a play entity.
+		/// </summary>
+		Player = 1
+	}
+}
---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/Identity/PlayerEntityIdentity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Creates a new <see cref="IEntityIdentity"/> for a player
+	/// entity.
+	/// </summary>
+	public sealed class PlayerEntityIdentity : EntityIdentity
+	{
+		/// <inheritdoc />
+		public PlayerEntityIdentity(byte entityId) 
+			: base(entityId, EntityType.Player)
+		{
+
+		}
+	}
+}
