655c4824c45b5846a7cf3e7dbef28ab1c7060015
HelloKitty <andrew.blakely@ymail.com>
2017-10-17 07:39:27 -0500

    Added FinishedWarpAck handler for client

---
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Command/Warping/BlockOtherClientAlertedExistenceEventhandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SceneJect.Common;
+using Sirenix.Serialization;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public sealed class BlockOtherClientAlertedExistenceEventhandler : Command60Handler<Sub60FinishedWarpAckCommand> //we don't need context
+	{
+		[Inject]
+		private INetworkPlayerFactory PlayerFactory { get; }
+
+		[Inject]
+		private IUnitScalerStrategy UnitScaler { get; }
+
+		/// <inheritdoc />
+		protected override Task HandleSubMessage(IClientMessageContext<PSOBBGamePacketPayloadClient> context, Sub60FinishedWarpAckCommand command)
+		{
+			float rotation = UnitScaler.ScaleYRotation(command.Rotation);
+			Vector3 position = UnitScaler.Scale(command.Position);
+
+			//TODO: We should check the ZoneId being sent AND if we already know the player. We shouldn't but we should still verify
+			PlayerFactory.CreatePlayer(command.ClientId, position, Quaternion.AngleAxis(rotation, Vector3.up));
+
+			return Task.CompletedTask;
+		}
+	}
+}
