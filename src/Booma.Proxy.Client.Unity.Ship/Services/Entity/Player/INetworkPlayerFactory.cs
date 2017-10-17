78a373b39c22d3d7959d6b23f18fb5bdd7eaf428
HelloKitty <andrew.blakely@ymail.com>
2017-10-17 01:35:55 -0500

    Added INetworkPlayerFactory

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/Player/INetworkPlayerFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Contract for a factory that creates <see cref="INetworkPlayer"/>s.
+	/// </summary>
+	public interface INetworkPlayerFactory
+	{
+		/// <summary>
+		/// Creates a new <see cref="INetworkPlayer"/> instance with the associated <see cref="id"/>.
+		/// This is just the data model and networking component reference for the player. It is not the character
+		/// nor the character data.
+		/// </summary>
+		/// <param name="id">The id.</param>
+		/// <returns>A new <see cref="INetworkPlayer"/>.</returns>
+		INetworkPlayer CreatePlayer(int id);
+
+		/// <summary>
+		/// Creates a new <see cref="INetworkPlayer"/> instance with the associated <see cref="id"/>.
+		/// This is just the data model and networking component reference for the player. It is not the character
+		/// nor the character data.
+		/// 
+		/// Uses the provided rotation and position data.
+		/// </summary>
+		/// <param name="id">The id.</param>
+		/// <param name="position">The position to create the <see cref="GameObject"/> at.</param>
+		/// <param name="rotation">The rotation to set for the created <see cref="GameObject"/>.</param>
+		/// <returns>A new <see cref="INetworkPlayer"/>.</returns>
+		INetworkPlayer CreatePlayer(int id, Vector3 position, Quaternion rotation);
+
+		/// <summary>
+		/// Creates a new <see cref="INetworkPlayer"/> instance for the local player with the associated assigned id.
+		/// This is just the data model and the networking component reference for the player. It is not the character nor
+		/// the character data.
+		/// </summary>
+		/// <returns></returns>
+		INetworkPlayer CreateLocalPlayer();
+	}
+}
