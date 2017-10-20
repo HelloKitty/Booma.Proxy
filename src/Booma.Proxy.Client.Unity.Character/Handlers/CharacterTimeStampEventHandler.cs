097a865f8f8b3595933b71e3812e3d7581b830c2
HelloKitty <andrew.blakely@ymail.com>
2017-10-20 01:33:56 -0500

    Added B1 beats timestamp handling

---
+++ src/Booma.Proxy.Client.Unity.Character/Handlers/CharacterTimeStampEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public sealed class CharacterTimeStampEventHandler : GameMessageHandler<CharacterTimestampEventPayload>
+	{
+		/// <inheritdoc />
+		public override Task HandleMessage(IClientMessageContext<PSOBBGamePacketPayloadClient> context, CharacterTimestampEventPayload payload)
+		{
+			//TODO: How should we account for latency between the server
+			//See: https://en.wikipedia.org/wiki/Swatch_Internet_Time#Calculation_from_UTC.2B1
+			DateTime serverTime = DateTimeOffset
+				.Parse(payload.Timestamp)
+				.UtcDateTime
+				.ToUniversalTime();
+
+			//Set the start beat time for use during this session
+			//beats = (UTC+1seconds + (UTC+1minutes * 60) + (UTC+1hours * 3600)) / 86.4
+			TimeService.StartBeatsTime = (serverTime.Second + (serverTime.Minute * 60) + (serverTime.Hour * 3600)) / 86.4d;
+
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"CurrentBeat: {TimeService.StartBeatsTime % 1000}");
+
+			return Task.CompletedTask;
+		}
+	}
+}
