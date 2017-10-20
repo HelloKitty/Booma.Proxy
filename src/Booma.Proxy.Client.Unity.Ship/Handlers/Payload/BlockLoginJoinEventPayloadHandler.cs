2b0c0095c1573ae49dd5f30d12991e28e18c7952
HelloKitty <andrew.blakely@ymail.com>
2017-10-19 19:33:11 -0500

    Extended the LobbyJoinEvent to support multiple lobby maps

--- src/Booma.Proxy.Client.Unity.Ship/Handlers/Payload/BlockLoginJoinEventPayloadHandler.cs
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Payload/BlockLoginJoinEventPayloadHandler.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SceneJect.Common;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
@@ -11,7 +13,7 @@ using UnityEngine.SceneManagement;
 namespace Booma.Proxy
 {
 	[Injectee]
-	public sealed class BlockLoginJoinEventPayloadHandler : GameMessageHandler<BlockLobbyJoinEventPayload>
+	public class BlockLoginJoinEventPayloadHandler : GameMessageHandler<BlockLobbyJoinEventPayload>
 	{
 		//TODO: Is it ok to reuse this?
 		[Inject]
@@ -20,14 +22,33 @@ namespace Booma.Proxy
 		[SerializeField]
 		private UnityEvent OnLobbyJoinEvent;
 
-		[SerializeField]
-		private int TestLobbyScene;
+		[PropertyTooltip("Should be a mapping between the lobby number and their scene files.")]
+		[OdinSerialize]
+		private Dictionary<int, string> LobbyNumberToSceneNameMap { get; set; }
+
+		protected virtual void Start()
+		{
+			base.Start();
+
+			if(LobbyNumberToSceneNameMap == null || !LobbyNumberToSceneNameMap.Any())
+				throw new InvalidOperationException("Must initialize lobby id to scene mapping.");
+		}
 
 		/// <inheritdoc />
 		public override Task HandleMessage(IClientMessageContext<PSOBBGamePacketPayloadClient> context, BlockLobbyJoinEventPayload payload)
 		{
 			if(Logger.IsDebugEnabled)
 				Logger.Debug($"**Handling**: {nameof(BlockLobbyJoinEventPayload)}");
+
+			//We need to make sure the lobby scene is registered
+			if(!LobbyNumberToSceneNameMap.ContainsKey(payload.LobbyNumber))
+			{
+				string lobbyError = $"Tried to enter Lobby: {payload.LobbyNumber} but no lobby for that id was registered.";
+				if(Logger.IsErrorEnabled)
+					Logger.Error(lobbyError);
+
+				throw new InvalidOperationException(lobbyError);
+			}
 
 			//Just set the old char slot to the clientid
 			//It's basically like a slot, like a lobby or party slot.
@@ -38,7 +59,7 @@ namespace Booma.Proxy
 			//TODO: Handle multiple different lobby scenes
 			//Now we need to load the actual lobby
 			//Doing so will require us to load a new lobby scene
-			SceneManager.LoadSceneAsync(TestLobbyScene).allowSceneActivation = true;
+			SceneManager.LoadSceneAsync(LobbyNumberToSceneNameMap[payload.LobbyNumber]).allowSceneActivation = true;
 
 			//Don't send anything here, the server will send a 0x60 0x6F after this
 			return Task.CompletedTask;
