ffd5373b5411cf8f8dc6c4481c8ee4d6b3b89a00
HelloKitty <andrew.blakely@ymail.com>
2017-10-21 05:27:13 -0500

    Added testing gamelist request component

---
+++ src/Booma.Proxy.Client.Unity.Ship/Components/GameListRequestSender.cs
@@ -0,0 +1,33 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+using Unitysync.Async;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Sends the game list request to the server.
+	/// </summary>
+	public sealed class GameListRequestSender : NetworkRequestSender
+	{
+		[Button("Send Gamelist Request")]
+		public void SendGameListRequest()
+		{
+			//Sends the request and gets the game list response in the continuation method
+			SendServiceAsync.SendRequestAsync<BlockGameListResponsePayload>(new BlockGameListRequestPayload())
+				.UnityAsyncContinueWith(this, HandleGameListResponse);
+		}
+
+		private void HandleGameListResponse(BlockGameListResponsePayload response)
+		{
+			//TODO: This is just for testing, we try to join the first one list
+			GameListEntry entry = response.GameEntries.First();
+
+			SendService.SendMessage(new SharedMenuSelectionRequestPayload(entry.Listing));
+		}
+	}
+}
---
+++ src/Booma.Proxy.Client.Unity.Ship/Components/NetworkRequestSender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SceneJect.Common;
+using Sirenix.OdinInspector;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Base for network request senders.
+	/// </summary>
+	[Injectee]
+	public abstract class NetworkRequestSender : SerializedMonoBehaviour
+	{
+		/// <summary>
+		/// The network send service used to send the messages
+		/// in the request sender.
+		/// </summary>
+		[Inject]
+		protected IClientPayloadSendService<PSOBBGamePacketPayloadClient> SendService { get; }
+
+		/// <summary>
+		/// The network send service used to send the messages
+		/// in the request sender.
+		/// </summary>
+		[Inject]
+		protected IClientRequestSendService<PSOBBGamePacketPayloadClient> SendServiceAsync { get; }
+	}
+}
