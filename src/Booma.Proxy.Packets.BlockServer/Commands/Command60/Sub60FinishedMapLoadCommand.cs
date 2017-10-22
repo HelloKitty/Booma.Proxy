151095d38cf48b4f4b0eff0f8c43dbadb62a2867
HelloKitty <andrew.blakely@ymail.com>
2017-10-22 06:59:13 -0500

    Added potential fields to 0x60 0x3B MapLoad

--- src/Booma.Proxy.Packets.BlockServer/Commands/Command60/Sub60FinishedMapLoadCommand.cs
+++ src/Booma.Proxy.Packets.BlockServer/Commands/Command60/Sub60FinishedMapLoadCommand.cs
@@ -12,10 +12,24 @@ namespace Booma.Proxy
 	/// </summary>
 	[WireDataContract]
 	[SubCommand60(SubCommand60OperationCode.SUBCMD_LOAD_3B)]
-	public sealed class Sub60FinishedMapLoadCommand : BaseSubCommand60
+	public sealed class Sub60FinishedMapLoadCommand : BaseSubCommand60, IMessageContextIdentifiable
 	{
 		//Empty command that just notifies other players of finished loading map
 
+		/// <inheritdoc />
+		[WireMember(1)]
+		public byte Identifier { get; }
+		
+		//TODO: What is this? Could be the map we loaded?
+		[WireMember(2)]
+		private byte unk { get; }
+
+		public Sub60FinishedMapLoadCommand(byte identifier)
+		{
+			Identifier = identifier;
+		}
+
+		//Serialzier ctor
 		public Sub60FinishedMapLoadCommand()
 		{
 			
