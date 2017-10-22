108b325c9281f4ff723f3955a1b25ce0b81d8e4b
HelloKitty <andrew.blakely@ymail.com>
2017-10-22 13:31:25 -0500

    Added GameBeginWarp and GameJoin handlers

---
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Command/Warping/BlockGameBeginWarpEventPayloadHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SceneJect.Common;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Command handler that handles the game/62 version of the <see cref="Sub60ClientWarpBeginEventCommand"/>.
+	/// This one uses a similar one called <see cref="Sub62ClientWarpBeginEventCommand"/> for games
+	/// </summary>
+	[Injectee]
+	public sealed class BlockGameBeginWarpEventPayloadHandler : Command62Handler<Sub62ClientWarpBeginEventCommand>
+	{
+		[Inject]
+		private INetworkPlayerFactory PlayerFactory { get; }
+
+		[Inject]
+		private IUnitScalerStrategy ScalerService { get; }
+
+		protected override async Task HandleSubMessage(IClientMessageContext<PSOBBGamePacketPayloadClient> context, Sub62ClientWarpBeginEventCommand command)
+		{
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"Recieved: {this.MessageName()} about to create local player.");
+
+			INetworkPlayer player = null;
+			try
+			{
+				//TODO: Is this where we should do this?
+				//We need to create the player represenation here
+				player = PlayerFactory.CreateLocalPlayer();
+			}
+			catch(Exception e)
+			{
+				if(Logger.IsErrorEnabled || Logger.IsFatalEnabled)
+					Logger.Fatal($"Failed to create network player. Exception: {e.Message} \n\n Stacktrace: {e.StackTrace}");
+				throw;
+			}
+
+			//TODO: Handle the zoneid better
+			await context.PayloadSendService.SendMessage(new Sub60TeleportToPositionCommand(player.Identity.EntityId, ScalerService.UnScale(player.Transform.Position).ToNetworkVector3()).ToPayload());
+			await context.PayloadSendService.SendMessage(new Sub60WarpToNewAreaCommand(player.Identity.EntityId, 0).ToPayload()); //pioneer 2
+			await context.PayloadSendService.SendMessage(new Sub60FinishedMapLoadCommand(player.Identity.EntityId).ToPayload());
+
+			//This tells everyone else we're doing bursting
+			await context.PayloadSendService.SendMessage(new BlockFinishedGameBurstingRequestPayload());
+		}
+	}
+}
--- src/Booma.Proxy.Client.Unity.Ship/Handlers/Payload/BlockGameJoinEventPayloadHandler.cs
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Payload/BlockGameJoinEventPayloadHandler.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SceneJect.Common;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 namespace Booma.Proxy
 {
+	[Injectee]
 	public sealed class BlockGameJoinEventPayloadHandler : GameMessageHandler<BlockGameJoinEventPayload>
 	{
+		[Inject]
+		private IUnitScalerStrategy UnitScaler { get; }
+
+		[Inject]
+		private ICharacterSlotSelectedModel SlotModel { get; }
+
+		/// <summary>
+		/// Event broadcast right before we load the game scene.
+		/// </summary>
+		[Tooltip("Invoked before the game scene is loaded.")]
+		[SerializeField]
+		private UnityEvent OnBeforeGameJoin;
+
+		//TODO: Implement proper scene/game loading
+		public int TestGameSceneIndex = 0;
+
 		/// <inheritdoc />
-		public override Task HandleMessage(IClientMessageContext<PSOBBGamePacketPayloadClient> context, BlockGameJoinEventPayload payload)
+		public override async Task HandleMessage(IClientMessageContext<PSOBBGamePacketPayloadClient> context, BlockGameJoinEventPayload payload)
 		{
 			if(Logger.IsInfoEnabled)
 			{
@@ -21,7 +42,30 @@ namespace Booma.Proxy
 				Logger.Info($"{payload.Settings}");
 			}
 
-			return Task.CompletedTask;
+			//We should init the slot model with our new identifer
+			SlotModel.SlotSelected = payload.Identifier;
+
+			//Then just like joining a lobby we should
+			//broadcast that we're about to join a game
+			//then load the scene responsible for handling the game we're entering.
+
+			OnBeforeGameJoin?.Invoke();
+
+			//TODO: Handle multiple different lobby scenes
+			//Now we need to load the game/amp (probably pioneer 2 but I don't think it HAAS to be)
+			SceneManager.LoadSceneAsync(TestGameSceneIndex).allowSceneActivation = true;
+
+			//Don't send anything here, the server will send a 0x71 to let us know we should spawn
+
+
+			//TODO: This is just test code to check if we can join a game
+			/*await context.PayloadSendService.SendMessage(new Sub60TeleportToPositionCommand(payload.Identifier, UnitScaler.UnScale(TestSpawnPoint).ToNetworkVector3()).ToPayload());
+			await context.PayloadSendService.SendMessage(new Sub60WarpToNewAreaCommand(payload.Identifier, 0).ToPayload()); //pioneer 2
+			await context.PayloadSendService.SendMessage(new Sub60FinishedMapLoadCommand(payload.Identifier).ToPayload());
+
+			await context.PayloadSendService.SendMessage(new BlockFinishedGameBurstingRequestPayload());*/
+
+			//If there are others in the zone we should broadcast a 20
 		}
 	}
 }
