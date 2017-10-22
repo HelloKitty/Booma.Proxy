a8f08e8cdc09dd1e6c6564e9e5931cabe2d53c67
HelloKitty <andrew.blakely@ymail.com>
2017-10-22 06:55:42 -0500

    Implemented sub60 0x3B MapLoaded command

---
+++ src/Booma.Proxy.Packets.BlockServer/Commands/Command60/Sub60FinishedMapLoadCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Payload sent by the client when it has finished loading the map.
+	/// </summary>
+	[WireDataContract]
+	[SubCommand60(SubCommand60OperationCode.SUBCMD_LOAD_3B)]
+	public sealed class Sub60FinishedMapLoadCommand : BaseSubCommand60
+	{
+		//Empty command that just notifies other players of finished loading map
+
+		public Sub60FinishedMapLoadCommand()
+		{
+			
+		}
+	}
+}
