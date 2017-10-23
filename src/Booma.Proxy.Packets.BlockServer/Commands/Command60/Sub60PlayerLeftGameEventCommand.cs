d3ee2741d95506ee0b861449b081f1c9a4b6448f
HelloKitty <andrew.blakely@ymail.com>
2017-10-22 20:11:48 -0500

    Added 0x60 0x66 GameLeftEvent

---
+++ src/Booma.Proxy.Packets.BlockServer/Commands/Command60/Sub60PlayerLeftGameEventCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	//Syl: https://github.com/Sylverant/ship_server/blob/b3bffc84b558821ca2002775ab2c3af5c6dde528/src/packets.h#L537
+	/// <summary>
+	/// Payload sent when another player leaves the game.
+	/// </summary>
+	[WireDataContract]
+	[SubCommand60(SubCommand60OperationCode.GAME_LEAVE_TYPE)]
+	public sealed class Sub60PlayerLeftGameEventCommand : BaseSubCommand60, IMessageContextIdentifiable
+	{
+		/*uint8_t client_id;
+		uint8_t leader_id;
+		uint16_t padding;*/
+		
+		/// <inheritdoc />
+		[WireMember(1)]
+		public byte Identifier { get; }
+
+		//TODO: Why do we need this?
+		[WireMember(2)]
+		private byte LeaderId { get; }
+
+		//TODO: Is this really unused as Syl says?
+		[WireMember(3)]
+		private short unk { get; }
+
+		//Serializer ctor
+		private Sub60PlayerLeftGameEventCommand()
+		{
+			
+		}
+	}
+}
