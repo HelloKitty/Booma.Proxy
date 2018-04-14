6378e8b4b2563a7bdcd5fe98db3b1600c3600213
HelloKitty <andrew.blakely@ymail.com>
2018-04-14 18:14:22 -0500

    Added MapPathBuilder

---
+++ src/Booma.Proxy.Client.Unity.Map/MapPathBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma
+{
+	/// <summary>
+	/// Object that can build a filename from map and variation.
+	/// </summary>
+	public sealed class MapPathBuilder
+	{
+		public string RootPath { get; }
+
+		//WARNING: Must use .bytes extension in Unity3D if we want to load it as a binary file.
+		/// <summary>
+		/// The extension of the map file.
+		/// Recommended to use ".bytes" for Unity3D asset loading.
+		/// </summary>
+		public string Extension { get; }
+
+		public MapPathBuilder(string rootPath = @"/maps/", string extension = ".dat")
+		{
+			if(string.IsNullOrWhiteSpace(extension)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(extension));
+			if(string.IsNullOrWhiteSpace(rootPath)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(rootPath));
+
+			RootPath = rootPath;
+			Extension = extension;
+		}
+
+		//TODO: Should we make generic? Should we add overloads?
+		public string GenerateFileName(Episode1Map map, int variation)
+		{
+			return $"map_{map.ToString().ToLower()}_{variation:00}";
+		}
+
+		public string GeneratePath(Episode1Map map, int variation)
+		{
+			return Path.Combine(RootPath, $"{GenerateFileName(map, variation)}{Extension}");
+		}
+	}
+}
---
+++ src/Booma.Proxy.Common/Enums/Episode1Map.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma
+{
+	/// <summary>
+	/// Enumeration of Episode 1 maps.
+	/// </summary>
+	public enum Episode1Map
+	{
+		/// <summary>
+		/// Pioneer2 for episode 1.
+		/// map_city00_00
+		/// </summary>
+		City00 = 0,
+
+		/// <summary>
+		/// TODO: ?
+		/// </summary>
+		Unknown1 = 1,
+
+		/// <summary>
+		/// TODO: ?
+		/// </summary>
+		Unknown2 = 2,
+
+		/// <summary>
+		/// Forest 01.
+		/// map_forest01_00
+		/// </summary>
+		Forest01 = 3,
+	}
+}
