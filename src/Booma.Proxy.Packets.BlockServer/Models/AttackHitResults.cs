dc89056c17c013e6703676046a140a44e940f4c3
HelloKitty <andrew.blakely@ymail.com>
2018-09-26 11:48:53 -0500

    Refactored MapObjectId/AttackHitResult opcode

--- src/Booma.Proxy.Packets.BlockServer/Models/AttackHitResults.cs
+++ src/Booma.Proxy.Packets.BlockServer/Models/AttackHitResults.cs
@@ -1,4 +1,6 @@
-﻿using FreecraftCore.Serializer;
+﻿using System;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
 
 namespace Booma.Proxy
 {
@@ -9,41 +11,13 @@ namespace Booma.Proxy
 		[WireMember(1)]
 		public short unk1 { get; }
 
-		/// <summary>
-		/// Two byte UID for the game itself that identifies
-		/// an object with the format of:
-		/// ABCC where CC is the ID of the object in the floor/area
-		/// and BCC is the index of the object in a flat array serverside and clientside
-		/// and A is the object type.
-		/// </summary>
 		[WireMember(2)]
-		private short ObjectIdentifier { get; }
-
-		/// <summary>
-		/// The floor ID of the object. Unique per floor.
-		/// </summary>
-		public byte ObjectFloorId => (byte)(ObjectIdentifier & 0xFF);
-
-		/// <summary>
-		/// The index of the object on the serverside/clientside.
-		/// </summary>
-		public short ObjectIndex => (short)(ObjectIdentifier & 0xFFF);
-
-		/// <summary>
-		/// The type of the object hit.
-		/// </summary>
-		public ObjectHitType ObjectType => (ObjectHitType)((ObjectIdentifier & 0xF000) >> 12);
+		public MapObjectIdentifier ObjectIdentifier { get; }
 
 		/// <inheritdoc />
-		public AttackHitResults(short objectIdentifier)
+		public AttackHitResults([NotNull] MapObjectIdentifier objectIdentifier)
 		{
-			ObjectIdentifier = objectIdentifier;
-		}
-
-		public AttackHitResults(ObjectHitType objectType, byte floorId, byte objectId)
-		{
-			//We have to bitshift these together, since the entry is sent packed like this.
-			ObjectIdentifier = (short)((short)objectType << 12 + floorId + objectId << 8);
+			ObjectIdentifier = objectIdentifier ?? throw new ArgumentNullException(nameof(objectIdentifier));
 		}
 
 		private AttackHitResults()
