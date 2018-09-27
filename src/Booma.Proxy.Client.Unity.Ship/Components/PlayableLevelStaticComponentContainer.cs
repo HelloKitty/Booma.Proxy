03b125be3254c99aa26f47a823c2f641be7263fb
HelloKitty <andrew.blakely@ymail.com>
2018-09-27 11:38:10 -0500

    Added some hacky Dependency Injection additive scene load stuff

---
+++ src/Booma.Proxy.Client.Unity.Ship/Components/PlayableLevelStaticComponentContainer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SceneJect.Common;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Booma.Proxy
+{
+	[Injectee]
+	public sealed class PlayableLevelStaticComponentContainer : SerializedMonoBehaviour
+	{
+		/// <summary>
+		/// Serialized pre-computed field of all monobehaviors
+		/// that will require injection at scene load time.
+		/// </summary>
+		[SerializeField]
+		private MonoBehaviour[] Behaviors;
+
+		void OnEnable()
+		{
+			SceneManager.sceneLoaded += OnSceneLoaded;
+		}
+
+		void OnDisable()
+		{
+			SceneManager.sceneLoaded -= OnSceneLoaded;
+		}
+
+		private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+		{
+			if(GlobalGameServices.InjectionService is null)
+				throw new InvalidOperationException($"{nameof(GlobalGameServices.InjectionService)} in {nameof(GlobalGameServices)} is null.");
+
+			//Just help inject the services into the referenced behaviors.
+			GlobalGameServices.InjectionService.InjectDependencies(Behaviors);
+		}
+
+		[Button]
+		void CollectStaticInjectableMonoBehaviors()
+		{
+			var injecteeLocator = new InjecteeLocator<MonoBehaviour>();
+
+			Behaviors = injecteeLocator.ToArray();
+		}
+	}
+}
