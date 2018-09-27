32d10dd7f9c2ba181acad49397bf57a50f9635be
HelloKitty <andrew.blakely@ymail.com>
2018-09-27 03:31:22 -0500

    Fix 0x60 0x46 attackhitresult command size

--- src/Booma.Proxy.Packets.BlockServer/Commands/Command60/Sub60PlayerFinishedAttackStepEvent.cs
+++ src/Booma.Proxy.Packets.BlockServer/Commands/Command60/Sub60PlayerFinishedAttackStepEvent.cs
@@ -13,7 +13,7 @@ namespace Booma.Proxy
 	/// </summary>
 	[WireDataContract]
 	[SubCommand60(SubCommand60OperationCode.AttackStepFinished)]
-	public sealed class Sub60PlayerFinishedAttackStepEvent : BaseSubCommand60, IMessageContextIdentifiable
+	public sealed class Sub60PlayerFinishedAttackStepEvent : BaseSubCommand60, IMessageContextIdentifiable, ISerializationEventListener
 	{
 		//TODO: I haven't verified that this is the identifier for the client BUT it probably has to be.
 		/// <inheritdoc />
@@ -39,14 +39,39 @@ namespace Booma.Proxy
 
 		//TODO: What is the last two bytes of this packet? Seems always 0, not from crypto padding.
 		[WireMember(4)]
-		private short unk2 { get; }
+		private short unk2 { get; } = 0;
+
+		/// <inheritdoc />
+		public Sub60PlayerFinishedAttackStepEvent(byte identifier, params AttackHitResult[] hitResults)
+		{
+			Identifier = identifier;
+			_HitResults = hitResults;
+
+			//We only set the command header before serialization because
+			//it's dynamic and can't be statically set and is only needed when we're about to serialize
+		}
 
 		/// <summary>
 		/// Serializer ctor.
 		/// </summary>
 		private Sub60PlayerFinishedAttackStepEvent()
 		{
-			
+			//We only set the command header before serialization because
+			//it's dynamic and can't be statically set and is only needed when we're about to serialize
+		}
+
+		/// <inheritdoc />
+		public void OnBeforeSerialization()
+		{
+			//Command size is header size + payload size /4
+			//header + lengthprefix size + hits size + final 2 bytes + 2 to avoid truncation
+			CommandSize = (byte)((2 + 2 + (_HitResults?.Length * 4 ?? 0) + 2 + 2) / 4);
+		}
+
+		/// <inheritdoc />
+		public void OnAfterDeserialization()
+		{
+
 		}
 	}
 }
