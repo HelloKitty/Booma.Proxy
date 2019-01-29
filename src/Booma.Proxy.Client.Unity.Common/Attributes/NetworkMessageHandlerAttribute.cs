727c1c775243cfdf5eecb22d4abc85266aea5c02
HelloKitty <andrew.blakely@ymail.com>
2019-01-29 06:16:37 -0600

    Added NetworkMessageHandlerAttribute

---
+++ src/Booma.Proxy.Client.Unity.Common/Attributes/NetworkMessageHandlerAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Metadata attributed used to encode information
+	/// about a network message handler.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+	public sealed class NetworkMessageHandlerAttribute : Attribute
+	{
+		/// <summary>
+		/// Indicates the scene type this handler is for.
+		/// </summary>
+		public HandlerSceneType SceneType { get; }
+
+		/// <inheritdoc />
+		public NetworkMessageHandlerAttribute(HandlerSceneType sceneType)
+		{
+			if(!Enum.IsDefined(typeof(HandlerSceneType), sceneType)) throw new InvalidEnumArgumentException(nameof(sceneType), (int)sceneType, typeof(HandlerSceneType));
+			SceneType = sceneType;
+		}
+	}
+
+	/// <summary>
+	/// Enumeration of scene types.
+	/// </summary>
+	public enum HandlerSceneType
+	{
+		LobbyDefault = 1,
+
+		LobbySoccer = 2,
+
+		Pioneer2 = 3,
+
+		RagolDefault = 4
+	}
+}
