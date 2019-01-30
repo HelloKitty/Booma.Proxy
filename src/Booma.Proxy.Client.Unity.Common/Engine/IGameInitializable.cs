e5c75fabe02e73aaff0fa78990ef81e64753922e
HelloKitty <andrew.blakely@ymail.com>
2019-01-30 11:40:34 -0600

    Added IGameInitializable from VRGuardians

---
+++ src/Booma.Proxy.Client.Unity.Common/Engine/IGameInitializable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Contracts for types that require initialization when
+	/// the game starts.
+	/// </summary>
+	public interface IGameInitializable
+	{
+		/// <summary>
+		/// Async OnGameInitialized function
+		/// called when the game initializes.
+		/// </summary>
+		/// <returns></returns>
+		Task OnGameInitialized();
+	}
+}
