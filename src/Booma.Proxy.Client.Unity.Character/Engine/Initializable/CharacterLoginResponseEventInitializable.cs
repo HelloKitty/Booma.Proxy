1df695dfa7c8fb9f30c55acfc097c53b7514b725
HelloKitty <andrew.blakely@ymail.com>
2019-01-30 15:16:22 -0600

    Added CharacterLoginResponseEventInitializable
    For handling login response on the character screen

---
+++ src/Booma.Proxy.Client.Unity.Character/Engine/Initializable/CharacterLoginResponseEventInitializable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GladNet;
+
+namespace Booma.Proxy
+{
+	//On login response subscriber for character selection screen.
+	[SceneTypeCreate(GameSceneType.CharacterSelectionScreen)]
+	public sealed class CharacterLoginResponseEventInitializable : IGameInitializable
+	{
+		private ILoginResponseEventSubscribable LoginResponseSubscriptionService { get; }
+
+		private IPeerPayloadSendService<PSOBBGamePacketPayloadClient> SendService { get; }
+
+		/// <inheritdoc />
+		public CharacterLoginResponseEventInitializable([NotNull] IPeerPayloadSendService<PSOBBGamePacketPayloadClient> sendService)
+		{
+			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
+		}
+
+		/// <inheritdoc />
+		protected void OnLoginSuccess(object sender, EventArgs eventArgs)
+		{
+			//The shared login handler on the character
+			//server needs to send
+			//Send the request for all
+			for(int i = 0; i < 4; i++) //TODO: Don't hardcode character limit.
+				SendService.SendMessage(new CharacterCharacterSelectionRequestPayload((byte)i, CharacterSelectionType.Preview));
+		}
+
+		/// <inheritdoc />
+		public Task OnGameInitialized()
+		{
+			LoginResponseSubscriptionService.OnLoginSuccess += OnLoginSuccess;
+			return Task.CompletedTask;
+		}
+	}
+}
