3ae71d29c5ff9bc1c498755692b0ec8d25a9b81f
HelloKitty <andrew.blakely@ymail.com>
2019-01-30 14:27:33 -0600

    Added ILoginResponseEventSubscribable

---
+++ src/Booma.Proxy.Client.Unity.SharedHandlers/HandlerEventInterfaces/ILoginResponseEventSubscribable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public interface ILoginResponseEventSubscribable
+	{
+		event EventHandler OnLoginSuccess;
+
+		event EventHandler OnLoginFailure;
+	}
+}
