0b5296ea1f02a668f9b9ef05c15796b1e20f788e
HelloKitty <andrew.blakely@ymail.com>
2019-01-30 14:12:04 -0600

    Added Unity async/await yield/extensions from VRGuardians

---
+++ src/Booma.Proxy.Client.Unity.Common/Engine/UnityExtended.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Booma.Proxy
+{
+	public static class UnityExtended
+	{
+		public static SynchronizationContext UnityMainThreadContext { get; private set; }
+
+		/// <summary>
+		/// Sets the <see cref="UnityMainThreadContext"/> with the current
+		/// thread's (caller thread) sync context.
+		/// </summary>
+		public static void InitializeSyncContext()
+		{
+			UnityMainThreadContext = SynchronizationContext.Current;
+		}
+	}
+}
