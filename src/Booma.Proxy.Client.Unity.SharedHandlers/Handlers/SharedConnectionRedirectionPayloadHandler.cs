fd54a20844f5f1391d39b4706beb78889da79885
HelloKitty <andrew.blakely@ymail.com>
2019-01-30 13:34:32 -0600

    Made OnConnectionRedirection event based in SharedConnectionRedirectionPayloadHandler

--- src/Booma.Proxy.Client.Unity.SharedHandlers/Handlers/SharedConnectionRedirectionPayloadHandler.cs
+++ src/Booma.Proxy.Client.Unity.SharedHandlers/Handlers/SharedConnectionRedirectionPayloadHandler.cs
@@ -12,7 +12,7 @@ using UnityEngine.Events;
 namespace Booma.Proxy
 {
 	[SceneTypeCreate(GameSceneType.ServerSelectionScreen)]
-	public class SharedConnectionRedirectionPayloadHandler : GameMessageHandler<SharedConnectionRedirectPayload>
+	public sealed class SharedConnectionRedirectionPayloadHandler : GameMessageHandler<SharedConnectionRedirectPayload>, IConnectionRedirectionEventSubscribable
 	{
 		private IFullCryptoInitializationService<byte[]> CryptoInitializer { get; }
 
@@ -20,6 +20,9 @@ namespace Booma.Proxy
 		/// Data model for connection details.
 		/// </summary>
 		private IGameConnectionEndpointDetails ConnectionEndpoint { get; }
+
+		/// <inheritdoc />
+		public event EventHandler OnConnectionRedirection;
 
 		/// <inheritdoc />
 		public SharedConnectionRedirectionPayloadHandler([NotNull] IGameConnectionEndpointDetails connectionEndpoint, [NotNull] IFullCryptoInitializationService<byte[]> cryptoInitializer, ILog logger) 
@@ -45,18 +48,13 @@ namespace Booma.Proxy
 			ConnectionEndpoint.IpAddress = payload.EndpointAddress.ToString();
 			ConnectionEndpoint.Port = payload.EndpointerPort;
 
-			await OnConnectionRedirected();
+			//Dispatch the redirection event.
+			OnConnectionRedirection?.Invoke(this, EventArgs.Empty);
 		}
 
 		private string BuildLoginDebugString(SharedConnectionRedirectPayload payload)
 		{
 			return $"Ip: {payload.EndpointAddress} Port: {payload.EndpointerPort}";
-		}
-
-		//Default version we don't do anything.
-		protected virtual Task OnConnectionRedirected()
-		{
-			return Task.CompletedTask;
 		}
 	}
 }
