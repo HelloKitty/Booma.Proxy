d874197045d6c8b4d58b3e776803ded26eaea302
HelloKitty <andrew.blakely@ymail.com>
2019-01-30 17:01:27 -0600

    Converted Tickable/Initializable to AutoCreated generic IoC

--- src/Booma.Proxy.Client.Unity.Consolidated/IoC/GameTickableRegisteration.cs
+++ src/Booma.Proxy.Client.Unity.Consolidated/IoC/GameTickableRegisteration.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace Booma.Proxy
 {
-    public sealed class Class1
-    {
+	public sealed class GameTickableRegisteration : AutoCreatedRegisteration<IGameTickable>
+	{
 		
-    }
+	}
 }
