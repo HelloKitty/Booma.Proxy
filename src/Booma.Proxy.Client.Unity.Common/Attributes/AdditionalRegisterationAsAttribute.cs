2666179b30875cb9ff48d5173b3888815b8a5fe8
HelloKitty <andrew.blakely@ymail.com>
2019-01-30 13:43:48 -0600

    Added AdditionalRegisterationAsAttribute

---
+++ src/Booma.Proxy.Client.Unity.Common/Attributes/AdditionalRegisterationAsAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+	public sealed class AdditionalRegisterationAsAttribute : Attribute
+	{
+		/// <summary>
+		/// The service type to register the marked type as.
+		/// </summary>
+		public Type ServiceType { get; }
+
+		/// <inheritdoc />
+		public AdditionalRegisterationAsAttribute([NotNull] Type serviceType)
+		{
+			//TODO: We can do some validated on if it's an invalid type or something that cannot be registered.
+
+			ServiceType = serviceType ?? throw new ArgumentNullException(nameof(serviceType));
+		}
+	}
+}
