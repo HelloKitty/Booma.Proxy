a3a5c9c2415da18c9fd1d2e2d846a9d2de997ec1
HelloKitty <andrew.blakely@ymail.com>
2019-01-30 13:31:42 -0600

    Added IConnectionRedirectionEventSubscribable

---
+++ src/Booma.Proxy.Client.Unity.SharedHandlers/HandlerEventInterfaces/IConnectionRedirectionEventSubscribable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Contract for Types that expose a subscribe-able event
+	/// for connection redirections.
+	/// </summary>
+	public interface IConnectionRedirectionEventSubscribable
+	{
+		event EventHandler OnConnectionRedirection;
+	}
+}
