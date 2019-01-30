df9ed6cf044bd289618bfa06f04041223c7e0539
HelloKitty <andrew.blakely@ymail.com>
2019-01-29 22:31:02 -0600

    Added character selection LoginResponsePayloadHandler
    This is strange, but I needed to handle the loss of the UnityEvent
    stuff.

---
+++ src/Booma.Proxy.Client.Unity.Character/Handlers/CharacterLoginResponsePayloadHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+
+namespace Booma.Proxy
+{
+	[NetworkMessageHandler(GameSceneType.CharacterSelectionScreen)]
+	public sealed class CharacterLoginResponsePayloadHandler : SharedLoginResponsePayloadHandler
+	{
+		private IPeerPayloadSendService<PSOBBGamePacketPayloadClient> SendService { get; }
+
+		/// <inheritdoc />
+		public CharacterLoginResponsePayloadHandler(IClientSessionDetails sessionDetails, ILog logger, [NotNull] IPeerPayloadSendService<PSOBBGamePacketPayloadClient> sendService) 
+			: base(sessionDetails, logger)
+		{
+			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
+		}
+
+		/// <inheritdoc />
+		protected override void OnLoginSuccess()
+		{
+			//The shared login handler on the character
+			//server needs to send
+			//Send the request for all
+			for(int i = 0; i < 4; i++) //TODO: Don't hardcode character limit.
+				SendService.SendMessage(new CharacterCharacterSelectionRequestPayload((byte)i, CharacterSelectionType.Preview));
+		}
+	}
+}
