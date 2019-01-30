837e265d6b94be2e0357723b71d76f7dba2d69d8
HelloKitty <andrew.blakely@ymail.com>
2019-01-29 18:45:40 -0600

    Added characterselect dependency registeration

---
+++ src/Booma.Proxy.Client.Unity.Character/IoC/CharacterSelectionScreenDependencyRegister.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using SceneJect.Common;
+
+namespace Booma.Proxy
+{
+	public sealed class CharacterSelectionScreenDependencyRegister : NonBehaviourDependency
+	{
+		/// <inheritdoc />
+		public override void Register(ContainerBuilder register)
+		{
+			//This is for anything that needs to touch the character UI.
+			register.RegisterType<CharacterTabUIElementsContext>()
+				.AsSelf();
+		}
+	}
+}
