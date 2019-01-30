65b0e90a80f61329cee1e7d0ac948890e5a1f8f7
HelloKitty <andrew.blakely@ymail.com>
2019-01-30 17:19:39 -0600

    Created derived PreBlockWelcomePayloadHandler

---
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Payload/PreBlockWelcomePayloadHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+
+namespace Booma.Proxy
+{
+	[SceneTypeCreate(GameSceneType.PreBlockBurstingScene)]
+	public sealed class PreBlockWelcomePayloadHandler : SharedWelcomePayloadHandler
+	{
+		/// <inheritdoc />
+		public PreBlockWelcomePayloadHandler(IFullCryptoInitializationService<byte[]> cryptoInitializer, IAuthenticationDetailsModel loginDetails, IClientSessionDetails sessionDetails, ILog logger) 
+			: base(cryptoInitializer, loginDetails, sessionDetails, logger)
+		{
+			this.AuthType = SharedLoginRequest93Payload.ServerType.Ship;
+		}
+	}
+}
