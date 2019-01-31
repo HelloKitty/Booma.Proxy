f995fb8059af00b98bd47eeb257fac9396b69700
HelloKitty <andrew.blakely@ymail.com>
2019-01-31 12:22:19 -0600

    Added LoginButtonNetworkConnectionController

---
+++ src/Booma.Proxy.Client.Unity.Authentication/Engine/Initializables/LoginButtonNetworkConnectionController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Features.AttributeFilters;
+using GladNet;
+using Guardians;
+
+namespace Booma.Proxy
+{
+	[SceneTypeCreate(GameSceneType.TitleScreen)]
+	public sealed class LoginButtonNetworkConnectionController : IGameInitializable
+	{
+		/// <summary>
+		/// UI element reference for the login button.
+		/// </summary>
+		private IUIButton LoginButtonElement { get; }
+
+		private IGameConnectionEndpointDetails ConnectDetails { get; }
+
+		private IConnectable Connectable { get; }
+
+		/// <inheritdoc />
+		public LoginButtonNetworkConnectionController(
+			[KeyFilter(UnityUIRegisterationKey.TitleLoginButton)] [NotNull] IUIButton loginButtonElement, 
+			[NotNull] IGameConnectionEndpointDetails connectDetails, 
+			[NotNull] IConnectable connectable)
+		{
+			LoginButtonElement = loginButtonElement ?? throw new ArgumentNullException(nameof(loginButtonElement));
+			ConnectDetails = connectDetails ?? throw new ArgumentNullException(nameof(connectDetails));
+			Connectable = connectable ?? throw new ArgumentNullException(nameof(connectable));
+		}
+
+		/// <inheritdoc />
+		public Task OnGameInitialized()
+		{
+			LoginButtonElement.AddOnClickListenerAsync(OnLoginButtonPresedAsync);
+			return Task.CompletedTask;
+		}
+
+		private Task OnLoginButtonPresedAsync()
+		{
+			//Assume that the connection details of this model have been validated before we
+			//reach this point, not this objects responsiability
+			return Connectable.ConnectAsync(ConnectDetails.IpAddress, ConnectDetails.Port);
+		}
+	}
+}
