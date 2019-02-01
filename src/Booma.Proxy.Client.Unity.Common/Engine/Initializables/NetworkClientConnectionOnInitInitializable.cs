5a25d713169f4522b5da6e28e7c4bdcb3e858d75
HelloKitty <andrew.blakely@ymail.com>
2019-01-31 19:48:37 -0600

    Added NetworkClientConnectionOnInitInitializable
    Works similar to the ConnectOnStart bool that used to be on the client.
    It's no longer on the client and needs to be done externally.

---
+++ src/Booma.Proxy.Client.Unity.Common/Engine/Initializables/NetworkClientConnectionOnInitInitializable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GladNet;
+
+namespace Booma.Proxy
+{
+	[SceneTypeCreate(GameSceneType.CharacterSelectionScreen)] //probably more than just the character screen.
+	public sealed class NetworkClientConnectionOnInitInitializable : IGameInitializable
+	{
+		private IConnectable Connectable { get; }
+
+		private IGameConnectionEndpointDetails ConnectionDetails { get; }
+
+		/// <inheritdoc />
+		public NetworkClientConnectionOnInitInitializable([NotNull] IConnectable connectable, [NotNull] IGameConnectionEndpointDetails connectionDetails)
+		{
+			Connectable = connectable ?? throw new ArgumentNullException(nameof(connectable));
+			ConnectionDetails = connectionDetails ?? throw new ArgumentNullException(nameof(connectionDetails));
+		}
+
+		/// <inheritdoc />
+		public Task OnGameInitialized()
+		{
+			//This initializable actually just
+			//connects a IConnectable with the provided game details.
+			return Connectable.ConnectAsync(ConnectionDetails.IpAddress, ConnectionDetails.Port);
+		}
+	}
+}
