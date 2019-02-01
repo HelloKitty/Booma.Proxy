180e75c5af30883da93eb4f0ed23de423e2c443c
HelloKitty <andrew.blakely@ymail.com>
2019-02-01 11:39:03 -0600

    Created a Loggyly Refit logging interface

---
+++ src/Booma.Proxy.Client.Unity.Common/Logging/ILoggylyRemoteLoggingService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public interface ILoggylyRemoteLoggingService
+	{
+		Task LogAsync([Body(BodySerializationMethod.UrlEncoded)] LogglyLoggingModel logData);
+	}
+
+	public sealed class LogglyLoggingModel
+	{
+		[AliasAs("LEVEL")]
+		public string Level { get; private set; }
+
+		[AliasAs("MESSAGE")]
+		public string Message { get; private set; }
+
+		[AliasAs("ID")]
+		public static string Identifier { get; private set; } = SystemInfo.deviceUniqueIdentifier;
+
+		/// <inheritdoc />
+		public LogglyLoggingModel([NotNull] string level, [NotNull] string message)
+		{
+			Level = level ?? throw new ArgumentNullException(nameof(level));
+			Message = message ?? throw new ArgumentNullException(nameof(message));
+		}
+	}
+}
