2a201bbfac6ff942c2ff4f303ca617fe567ea91c
HelloKitty <andrew.blakely@ymail.com>
2019-02-02 16:07:04 -0600

    Added BaseTwoComponentUnityUIAdapter

---
+++ src/Booma.Proxy.Client.Unity.Common/UI/Abstraction/Adapters/Base/BaseTwoComponentUnityUIAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guardians;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public abstract class BaseTwoComponentUnityUIAdapter<TAdaptedUnityEngineType, TAdaptedUnityEngineType2, TAdaptedToType> : BaseUnityUIAdapter<TAdaptedUnityEngineType, TAdaptedToType>
+	{
+		[SerializeField]
+		protected TAdaptedUnityEngineType2 _UnityUIObject2;
+
+		/// <summary>
+		/// The Unity engine UI object being adapted.
+		/// </summary>
+		protected TAdaptedUnityEngineType2 UnityUIObject2 => _UnityUIObject2;
+	}
+}
