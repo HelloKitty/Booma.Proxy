e206d19745e13c7bd1f37b48087a2ea5a52f2bcb
HelloKitty <andrew.blakely@ymail.com>
2019-02-02 16:07:26 -0600

    Added ILabeledButton UI interface

---
+++ src/Booma.Proxy.Client.Unity.Common/UI/Abstraction/Interface/ILabeledButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guardians;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// UI combination of <see cref="IUIButton"/>
+	/// and a label <see cref="IUIText"/>.
+	/// </summary>
+	public interface ILabeledButton : IUIButton, IUIText
+	{
+		
+	}
+}
