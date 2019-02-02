791fa4f3436369f85827c88c239f01e119d64669
HelloKitty <andrew.blakely@ymail.com>
2019-02-01 20:16:58 -0600

    Added IMarqueeTextChangedEventSubscribable

---
+++ src/Booma.Proxy.Client.Unity.SharedHandlers/HandlerEventInterfaces/IMarqueeTextChangedEventSubscribable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public interface IMarqueeTextChangedEventSubscribable
+	{
+		event EventHandler<MarqueeTextChangedEventArgs> OnMarqueeTextChangedEvent;
+	}
+
+	//You might wonder why we don't just send a string
+	//My concern is that there is more data in the packet
+	//so we may have to send that too at some point.
+	public sealed class MarqueeTextChangedEventArgs : EventArgs
+	{
+		public string ChangedText { get; }
+
+		/// <inheritdoc />
+		public MarqueeTextChangedEventArgs([NotNull] string changedText)
+		{
+			//Could potentially be empty? Let's not assume
+			ChangedText = changedText ?? throw new ArgumentNullException(nameof(changedText));
+		}
+	}
+}
