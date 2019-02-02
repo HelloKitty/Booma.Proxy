1de8281286bf91be5275515d3ee702428dbb90ae
HelloKitty <andrew.blakely@ymail.com>
2019-02-01 18:55:30 -0600

    Added IOnCharacterSelectionAcknowledgementEventSubscribable

---
+++ src/Booma.Proxy.Client.Unity.Character/HandlerEventInterfaces/IOnCharacterSelectionAcknowledgementEventSubscribable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public interface IOnCharacterSelectionAcknowledgementEventSubscribable
+	{
+		event EventHandler<CharacterSelectionAckEventArgs> OnCharacterSelectionAcknowledgementRecieved;
+	}
+
+	public sealed class CharacterSelectionAckEventArgs : EventArgs
+	{
+		public CharacterSelectionAckType AckType { get; }
+
+		public byte SlotId { get; }
+
+		/// <inheritdoc />
+		public CharacterSelectionAckEventArgs(CharacterSelectionAckType ackType, byte slotId)
+		{
+			if(!Enum.IsDefined(typeof(CharacterSelectionAckType), ackType)) throw new InvalidEnumArgumentException(nameof(ackType), (int)ackType, typeof(CharacterSelectionAckType));
+
+			AckType = ackType;
+			SlotId = slotId;
+		}
+	}
+}
