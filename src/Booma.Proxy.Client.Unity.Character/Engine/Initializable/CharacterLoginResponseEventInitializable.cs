7b593d1307a099b05a669b49e081cefa5b8ddd61
HelloKitty <andrew.blakely@ymail.com>
2019-02-01 22:11:00 -0600

    Rewrote CharacterLoginResponseEventInitializable to use BaseSingleEventListenerInitializable

--- src/Booma.Proxy.Client.Unity.Character/Engine/Initializable/CharacterLoginResponseEventInitializable.cs
+++ src/Booma.Proxy.Client.Unity.Character/Engine/Initializable/CharacterLoginResponseEventInitializable.cs
@@ -9,34 +9,29 @@ namespace Booma.Proxy
 {
 	//On login response subscriber for character selection screen.
 	[SceneTypeCreate(GameSceneType.CharacterSelectionScreen)]
-	public sealed class CharacterLoginResponseEventInitializable : IGameInitializable
+	public sealed class CharacterLoginResponseEventInitializable : BaseSingleEventListenerInitializable<ILoginResponseEventSubscribable, LoginResultEventArgs>
 	{
-		private ILoginResponseEventSubscribable LoginResponseSubscriptionService { get; }
-
 		private IPeerPayloadSendService<PSOBBGamePacketPayloadClient> SendService { get; }
 
 		/// <inheritdoc />
-		public CharacterLoginResponseEventInitializable([NotNull] IPeerPayloadSendService<PSOBBGamePacketPayloadClient> sendService, [NotNull] ILoginResponseEventSubscribable loginResponseSubscriptionService)
+		public CharacterLoginResponseEventInitializable(ILoginResponseEventSubscribable subscriptionService, [NotNull] IPeerPayloadSendService<PSOBBGamePacketPayloadClient> sendService) 
+			: base(subscriptionService)
 		{
 			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
-			LoginResponseSubscriptionService = loginResponseSubscriptionService ?? throw new ArgumentNullException(nameof(loginResponseSubscriptionService));
 		}
 
 		/// <inheritdoc />
-		protected void OnLoginSuccess(object sender, LoginResultEventArgs args)
+		protected override void OnEventFired(object source, LoginResultEventArgs args)
 		{
+			//Not interested if we failed
+			if(!args.isSuccessful)
+				return;
+
 			//The shared login handler on the character
 			//server needs to send
 			//Send the request for all
 			for(int i = 0; i < 4; i++) //TODO: Don't hardcode character limit.
 				SendService.SendMessage(new CharacterCharacterSelectionRequestPayload((byte)i, CharacterSelectionType.Preview));
-		}
-
-		/// <inheritdoc />
-		public Task OnGameInitialized()
-		{
-			LoginResponseSubscriptionService.OnLoginProcessResult += OnLoginSuccess;
-			return Task.CompletedTask;
 		}
 	}
 }
