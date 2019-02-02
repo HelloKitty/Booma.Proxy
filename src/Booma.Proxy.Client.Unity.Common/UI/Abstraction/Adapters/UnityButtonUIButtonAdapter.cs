2e68ec0c8ed20996b27c3a1fca32383007564d78
HelloKitty <andrew.blakely@ymail.com>
2019-02-02 14:43:59 -0600

    Rewrote UnityButtonUIButtonAdapter to forward to Non-monobehaviour Type

--- src/Booma.Proxy.Client.Unity.Common/UI/Abstraction/Adapters/UnityButtonUIButtonAdapter.cs
+++ src/Booma.Proxy.Client.Unity.Common/UI/Abstraction/Adapters/UnityButtonUIButtonAdapter.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
+using Booma.Proxy;
 using UnityEngine.UI;
 using Unitysync.Async;
 
@@ -11,43 +12,32 @@ namespace Guardians
 {
 	public sealed class UnityButtonUIButtonAdapter : BaseUnityUIAdapter<Button, IUIButton>, IUIButton
 	{
+		private UnityButtonUIButtonAdapterImplementation Adapter { get; set; }
+
+		//On awake we should just create the adapter for
+		//adaptation forwarding.
+		void Awake()
+		{
+			Adapter = new UnityButtonUIButtonAdapterImplementation(UnityUIObject);
+		}
+
 		/// <inheritdoc />
 		public void AddOnClickListener(Action action)
 		{
-			if(action == null) throw new ArgumentNullException(nameof(action));
-
-			UnityUIObject.onClick.AddListener(() => action());
+			Adapter.AddOnClickListener(action);
 		}
 
 		/// <inheritdoc />
 		public void AddOnClickListenerAsync(Func<Task> action)
 		{
-			if(action == null) throw new ArgumentNullException(nameof(action));
-
-			//Supporting async button events from the Unity engine button is abit complex.
-			AddOnClickListener(() => AsyncUnityEngineButtonCallbackHandler(action));
+			Adapter.AddOnClickListenerAsync(action);
 		}
 
 		/// <inheritdoc />
 		public bool IsInteractable
 		{
-			get => UnityUIObject.interactable;
-			set => UnityUIObject.interactable = value;
-		}
-
-		private void AsyncUnityEngineButtonCallbackHandler(Func<Task> action)
-		{
-			if(action == null) throw new ArgumentNullException(nameof(action));
-
-			//When this is called, the button has been clicked and we need async button handling.
-			//This will call the async Method, get the task and create a coroutine that awaits it (for exception handling purposes)
-			StartCoroutine(AsyncCallbackHandler(action()));
-		}
-
-		//TODO: Have UnitySync.Async support exception forwarding.
-		private void DummyMethod()
-		{
-
+			get => Adapter.IsInteractable;
+			set => Adapter.IsInteractable = value;
 		}
 	}
 }
