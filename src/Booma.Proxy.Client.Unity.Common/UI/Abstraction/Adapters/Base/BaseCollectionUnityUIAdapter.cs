f48b64e3bbf21025f78cf3e630427335f6fbf914
HelloKitty <andrew.blakely@ymail.com>
2019-02-02 16:27:28 -0600

    Implemented missing BaseCollectionUnityUIAdapter logic

--- src/Booma.Proxy.Client.Unity.Common/UI/Abstraction/Adapters/Base/BaseCollectionUnityUIAdapter.cs
+++ src/Booma.Proxy.Client.Unity.Common/UI/Abstraction/Adapters/Base/BaseCollectionUnityUIAdapter.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
 
 namespace Booma.Proxy
 {
@@ -12,6 +15,17 @@ namespace Booma.Proxy
 	/// </summary>
 	public sealed class BaseCollectionUnityUIAdapter<TAdaptedToType> : BaseUnityUI<IReadOnlyCollection<TAdaptedToType>>
 	{
-		
+		[OdinSerialize] //probably required due to the generic-ness.
+		[Tooltip("Order matters. This is the collection of elements to be exposed.")]
+		private TAdaptedToType[] Elements;
+
+		[Button]
+		public void ValidateComponent()
+		{
+			if(Elements == null)
+				Elements = new TAdaptedToType[0];
+
+			Elements = Elements.Where(e => e != null).ToArray();
+		}
 	}
 }
