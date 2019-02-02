e92668d48aebf032b7ce1b996f07cf4b94804a8f
HelloKitty <andrew.blakely@ymail.com>
2019-02-02 16:23:14 -0600

    Added BaseCollectionUnityUIAdapter

---
+++ src/Booma.Proxy.Client.Unity.Common/UI/Abstraction/Adapters/Base/BaseCollectionUnityUIAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Provider the ability for ordered collections of element Type <typeparamref name="TAdaptedToType"/>
+	/// to be adapted and registered together.
+	/// </summary>
+	public sealed class BaseCollectionUnityUIAdapter<TAdaptedToType> : BaseUnityUI<IReadOnlyCollection<TAdaptedToType>>
+	{
+		
+	}
+}
