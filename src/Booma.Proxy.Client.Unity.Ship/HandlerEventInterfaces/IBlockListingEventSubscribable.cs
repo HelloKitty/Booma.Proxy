0ba71e332754e954c969c0caf66d21e1d9d5b364
HelloKitty <andrew.blakely@ymail.com>
2019-02-02 17:48:44 -0600

    Added IBlockListingEventSubscribable

---
+++ src/Booma.Proxy.Client.Unity.Ship/HandlerEventInterfaces/IBlockListingEventSubscribable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public interface IBlockListingEventSubscribable
+	{
+		/// <summary>
+		/// Event fired when a block listing is recieved.
+		/// </summary>
+		event EventHandler<BlockListingDataRecievedEventArgs> OnBlockListingRecieved;
+
+		/// <summary>
+		/// Event that should be fired when a block list has been fully recieved.
+		/// </summary>
+		event EventHandler OnBlockListFinishedRecieving;
+	}
+
+	public sealed class BlockListingDataRecievedEventArgs
+	{
+		/// <summary>
+		/// The identifier for the menu option.
+		/// </summary>
+		public MenuItemIdentifier Identifier { get; }
+
+		public string BlockName { get; }
+
+		/// <inheritdoc />
+		public BlockListingDataRecievedEventArgs([NotNull] MenuItemIdentifier identifier, [NotNull] string blockName)
+		{
+			Identifier = identifier ?? throw new ArgumentNullException(nameof(identifier));
+			BlockName = blockName ?? throw new ArgumentNullException(nameof(blockName));
+		}
+	}
+}
