795122207cdcb4c663b39eb18089eb6cfbb1a8eb
HelloKitty <andrew.blakely@ymail.com>
2019-02-02 16:12:33 -0600

    Added UnityLabeledButtonAdapter

---
+++ src/Booma.Proxy.Client.Unity.Common/UI/Abstraction/Adapters/UnityLabeledButtonAdapter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// See <see cref="ILabeledButton"/>.
+	/// </summary>
+	public sealed class UnityLabeledButtonAdapter : BaseTwoComponentUnityUIAdapter<Button, Text, ILabeledButton>, ILabeledButton
+	{
+		private UnityTextUITextAdapterImplementation TextAdapter { get; set; }
+
+		private UnityButtonUIButtonAdapterImplementation ButtonAdapter { get; set; }
+
+		void Awake()
+		{
+			TextAdapter = new UnityTextUITextAdapterImplementation(UnityUIObject2);
+			ButtonAdapter = new UnityButtonUIButtonAdapterImplementation(UnityUIObject);
+		}
+
+		/// <inheritdoc />
+		public string Text
+		{
+			get => TextAdapter.Text;
+			set => TextAdapter.Text = value;
+		}
+
+		/// <inheritdoc />
+		public void AddOnClickListener(Action action)
+		{
+			ButtonAdapter.AddOnClickListener(action);
+		}
+
+		/// <inheritdoc />
+		public void AddOnClickListenerAsync(Func<Task> action)
+		{
+			ButtonAdapter.AddOnClickListenerAsync(action);
+		}
+
+		/// <inheritdoc />
+		public bool IsInteractable
+		{
+			get => ButtonAdapter.IsInteractable;
+			set => ButtonAdapter.IsInteractable = value;
+		}
+	}
+}
