5e4c4bb8799a6a10039d2d8167df33f3167a5337
HelloKitty <andrew.blakely@ymail.com>
2019-02-02 13:56:17 -0600

    Created a base-base UI Type for UI system

--- src/Booma.Proxy.Client.Unity.Common/UI/Abstraction/Adapters/Base/BaseUnityUIAdapter.cs
+++ src/Booma.Proxy.Client.Unity.Common/UI/Abstraction/Adapters/Base/BaseUnityUIAdapter.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Booma.Proxy;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using Unitysync.Async;
 
 namespace Guardians
 {
-	public abstract class BaseUnityUIAdapter<TAdaptedUnityEngineType, TAdaptedToType> : SerializedMonoBehaviour, IUIAdapterRegisterable
+	public abstract class BaseUnityUIAdapter<TAdaptedUnityEngineType, TAdaptedToType> : BaseUnityUI, IUIAdapterRegisterable
 	{
 		static BaseUnityUIAdapter()
 		{
@@ -62,35 +63,6 @@ namespace Guardians
 		protected virtual bool ValidateInitializedObject(TAdaptedUnityEngineType obj)
 		{
 			return obj != null;
-		}
-
-		/// <summary>
-		/// Can be called as a <see cref="StartCoroutine"/>
-		/// to track the result, and dispatch the exception/logging for, async tasks.
-		/// </summary>
-		/// <param name="task">The task to await.</param>
-		/// <returns></returns>
-		protected IEnumerator AsyncCallbackHandler(Task task)
-		{
-			if(task == null) throw new ArgumentNullException(nameof(task));
-
-			//This will wait until the task is complete
-			yield return new WaitForFuture(task);
-
-			if(task.IsFaulted)
-			{
-				StringBuilder builder = new StringBuilder(200);
-
-				if(task.Exception != null && task.Exception.InnerExceptions != null)
-					foreach(Exception inner in task.Exception?.InnerExceptions)
-					{
-						builder.Append($"\nMessage: {inner.Message}\nStack: {inner.StackTrace}");
-					}
-
-				UnityEngine.Debug.LogError($"Encounter exception from Button: {name} OnClickAsync: {builder.ToString()}");
-			}
-
-			//We don't need to do anything, task succeeded and is finished.
 		}
 	}
 }
