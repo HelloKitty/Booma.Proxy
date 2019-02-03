e4e621951a04994ab1db3df2c3887ad4e263da8c
HelloKitty <andrew.blakely@ymail.com>
2019-02-02 20:50:50 -0600

    Fix issue with CharacterToShipConnectionRedirectorEventListener; use IConnectionRedirector

--- src/Booma.Proxy.Client.Unity.Character/Engine/Initializable/CharacterToShipConnectionRedirectorEventListener.cs
+++ src/Booma.Proxy.Client.Unity.Character/Engine/Initializable/CharacterToShipConnectionRedirectorEventListener.cs
@@ -11,28 +11,22 @@ namespace Booma.Proxy
 	[SceneTypeCreate(GameSceneType.ServerSelectionScreen)]
 	public sealed class CharacterToShipConnectionRedirectorEventListener : BaseSingleEventListenerInitializable<IConnectionRedirectionEventSubscribable>
 	{
-		private IGameConnectionEndpointDetails ConnectionDetails { get; }
-
-		private IConnectable Connectable { get; }
+		private IConnectionRedirector Reconnector { get; }
 
 		private ILog Logger { get; }
 
 		/// <inheritdoc />
-		public CharacterToShipConnectionRedirectorEventListener(IConnectionRedirectionEventSubscribable subscriptionService, [NotNull] IGameConnectionEndpointDetails connectionDetails, [NotNull] IConnectable connectable, [NotNull] ILog logger) 
-			: base(subscriptionService)
+		public CharacterToShipConnectionRedirectorEventListener([NotNull] IConnectionRedirectionEventSubscribable subscriptionService, [NotNull] IConnectionRedirector reconnector, [NotNull] ILog logger) : base(subscriptionService)
 		{
-			ConnectionDetails = connectionDetails ?? throw new ArgumentNullException(nameof(connectionDetails));
-			Connectable = connectable ?? throw new ArgumentNullException(nameof(connectable));
+			Reconnector = reconnector ?? throw new ArgumentNullException(nameof(reconnector));
 			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
 		}
 
 		/// <inheritdoc />
 		protected override async void OnEventFired(object source, EventArgs args)
 		{
-			if(Logger.IsInfoEnabled)
-				Logger.Info($"Redirecting Connection: {ConnectionDetails.IpAddress}:{ConnectionDetails.Port}");
-
-			await Connectable.ConnectAsync(ConnectionDetails.IpAddress, ConnectionDetails.Port);
+			//We just wanna redirect, that's all.
+			await Reconnector.RedirectAsync();
 
 			//The way Character to Ship redirection works
 			//is that it shouldn't redirect again on next redirect
