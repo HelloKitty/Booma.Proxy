5249581ca9b2b98f3061a8ca014b5a1b6c048581
HelloKitty <andrew.blakely@ymail.com>
2019-02-03 12:35:18 -0600

    Send Disconnect on CharacterSelection ack
    Might be best to do this in the handler itself if it sees a valid
    selection.

--- src/Booma.Proxy.Client.Unity.Character/Engine/Initializable/CharacterSelectionResultEventListenerInitializable.cs
+++ src/Booma.Proxy.Client.Unity.Character/Engine/Initializable/CharacterSelectionResultEventListenerInitializable.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GladNet;
 using UnityEngine.SceneManagement;
 
 namespace Booma.Proxy
@@ -10,15 +11,17 @@ namespace Booma.Proxy
 	[SceneTypeCreate(GameSceneType.PreShipSelectionScene)]
 	public sealed class CharacterSelectionResultEventListenerInitializable : BaseSingleEventListenerInitializable<IOnCharacterSelectionAcknowledgementEventSubscribable, CharacterSelectionAckEventArgs>
 	{
+		private IPeerPayloadSendService<PSOBBGamePacketPayloadClient> SendService { get; }
+
 		/// <inheritdoc />
-		public CharacterSelectionResultEventListenerInitializable([NotNull] IOnCharacterSelectionAcknowledgementEventSubscribable characterAckSubscriptionService)
+		public CharacterSelectionResultEventListenerInitializable([NotNull] IOnCharacterSelectionAcknowledgementEventSubscribable characterAckSubscriptionService, [NotNull] IPeerPayloadSendService<PSOBBGamePacketPayloadClient> sendService)
 			: base(characterAckSubscriptionService)
 		{
-
+			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
 		}
 
 		/// <inheritdoc />
-		protected override void OnEventFired(object source, CharacterSelectionAckEventArgs args)
+		protected override async void OnEventFired(object source, CharacterSelectionAckEventArgs args)
 		{
 			if(args == null) throw new ArgumentNullException(nameof(args));
 
@@ -27,18 +30,22 @@ namespace Booma.Proxy
 				case CharacterSelectionAckType.BB_CHAR_ACK_UPDATE:
 					break;
 				case CharacterSelectionAckType.BB_CHAR_ACK_SELECT:
-					OnSelectionSuccessful();
+					await OnSelectionSuccessful();
 					break;
 				case CharacterSelectionAckType.BB_CHAR_ACK_NONEXISTANT:
 					break;
 			}
 		}
 
-		private void OnSelectionSuccessful()
+		private async Task OnSelectionSuccessful()
 		{
 			//The character selected was successfully selected
 			//so we should load the next scene.
 			//TODO: We should not hardcode scene id NOR should we directly do scene loading. Do it through a service
+
+			//We should disconnect first
+			//Client sends 05 here, can cause issues if we don't maybe?
+			await SendService.SendMessage(new SharedDisconnectionRequestPayload());
 
 			//We can just load right away.
 			SceneManager.LoadSceneAsync(3).allowSceneActivation = true;
