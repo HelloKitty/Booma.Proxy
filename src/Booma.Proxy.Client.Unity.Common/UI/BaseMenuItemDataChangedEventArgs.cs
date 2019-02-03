2e0f82178d59c307da6e2bc30dd941da07a7ed63
HelloKitty <andrew.blakely@ymail.com>
2019-02-03 10:05:48 -0600

    Created base menu data changed args

---
+++ src/Booma.Proxy.Client.Unity.Common/UI/BaseMenuItemDataChangedEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public class BaseMenuItemDataChangedEventArgs : EventArgs
+	{
+		/// <summary>
+		/// The identifier for the menu option.
+		/// </summary>
+		public MenuItemIdentifier Identifier { get; }
+
+		public string MenuItemName { get; }
+
+		/// <inheritdoc />
+		public BaseMenuItemDataChangedEventArgs(MenuItemIdentifier identifier, string menuItemName)
+		{
+			Identifier = identifier;
+			MenuItemName = menuItemName;
+		}
+	}
+}
