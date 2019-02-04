bca165b4b390d268aae735bba7a398aa75575f29
HelloKitty <andrew.blakely@ymail.com>
2019-02-03 23:30:12 -0600

    Added int-based EntityGuidMappable from VRGuardians

---
+++ src/Booma.Proxy.Common/Entity/EntityType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Enumeration of all Entity types.
+	/// </summary>
+	public enum EntityType : byte
+	{
+		/// <summary>
+		/// Represents a typeless entity.
+		/// </summary>
+		None = 0,
+
+		/// <summary>
+		/// Player entity.
+		/// </summary>
+		Player = 1,
+
+		/// <summary>
+		/// GameObject entity.
+		/// (Such as a Door or a Button)
+		/// </summary>
+		GameObject = 2,
+
+		Npc = 3,
+	}
+}
---
+++ src/Booma.Proxy.Common/Entity/IReadonlyEntityGuidMappable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma.Proxy
+{
+	public interface IReadonlyEntityGuidMappable<TValue> : IReadOnlyDictionary<int, TValue>
+	{
+		
+	}
+}
