6d8e41ff85322a7427f6d0b6e4b90d6d420ec3b9
HelloKitty <andrew.blakely@ymail.com>
2019-02-05 13:36:56 -0600

    Added MainCameraGrabToTransform

---
+++ src/Booma.Proxy.Client.Unity.Ship/Components/MainCameraGrabToTransform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Component will grab the Camera and parent it to
+	/// the specified transform.
+	/// </summary>
+	public sealed class MainCameraGrabToTransform : MonoBehaviour
+	{
+		/// <summary>
+		/// The Should be the transform the camera should be put in.
+		/// </summary>
+		[Required]
+		[SerializeField]
+		private Transform CameraPosition;
+
+		void Start()
+		{
+			//Make camera follow the transform.
+			Camera.main.transform.parent = CameraPosition;
+			Camera.main.transform.position = CameraPosition.position;
+		}
+	}
+}
