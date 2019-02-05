bdb7b83940ccac49e5bd8222d61e6ee58be3977e
HelloKitty <andrew.blakely@ymail.com>
2019-02-05 11:59:55 -0600

    Replaced lobby player factory with new WorldObject player factory

---
+++ src/Booma.Proxy.Client.Unity.Ship/Factory/LobbyLocalPlayerWorldObjectFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guardians;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	[SceneTypeCreate(GameSceneType.LobbyDefault)]
+	public sealed class LobbyLocalPlayerWorldObjectFactory : IFactoryCreatable<GameObject, LocalPlayerWorldRepresentationCreationContext>, IGameInitializable
+	{
+		private IRoomCollection Rooms { get; }
+
+		private IWorldObjectToEntityMappable WorldPlayerMap { get; }
+
+		private INetworkPlayerPrefabProvider PrefabProvider { get; }
+
+		/// <inheritdoc />
+		public LobbyLocalPlayerWorldObjectFactory([NotNull] IRoomCollection rooms, [NotNull] IWorldObjectToEntityMappable worldPlayerMap, [NotNull] INetworkPlayerPrefabProvider prefabProvider)
+		{
+			Rooms = rooms ?? throw new ArgumentNullException(nameof(rooms));
+			WorldPlayerMap = worldPlayerMap ?? throw new ArgumentNullException(nameof(worldPlayerMap));
+			PrefabProvider = prefabProvider ?? throw new ArgumentNullException(nameof(prefabProvider));
+		}
+
+		/// <inheritdoc />
+		public GameObject Create(LocalPlayerWorldRepresentationCreationContext context)
+		{
+			GameObject player = GameObject.Instantiate(PrefabProvider.LocalPlayerPrefab, context.SpawnData.AssociatedObject.Position, context.SpawnData.AssociatedObject.Rotation);
+
+			//TODO: How should we handle rooms?
+			//Rooms.DefaultRoom.TryEnter(player);
+			return player;
+		}
+
+		//TODO: This is kinda a hack to get into the scene
+		/// <inheritdoc />
+		public Task OnGameInitialized()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
