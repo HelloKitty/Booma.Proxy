64e24324510683219302f81f6619b2db51bd9ddc
HelloKitty <andrew.blakely@ymail.com>
2019-02-04 18:11:54 -0600

    Create reverse map from WorldObject to EntityGuid

---
+++ src/Booma.Proxy.Client.Unity.Common/Entity/IReadonlyWorldObjectToEntityMappable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	//TODO: Doc
+	public interface IReadonlyWorldObjectToEntityMappable : IReadOnlyDictionary<GameObject, int>
+	{
+		
+	}
+}
