e1a3999b3dd87d6d038b9bae99725079851fee40
HelloKitty <andrew.blakely@ymail.com>
2019-02-05 16:51:18 -0600

    Added IMovementInputChangedEventSubscribable and MovementInputChangedEventArgs

---
+++ src/Booma.Proxy.Client.Unity.Ship/Engine/Initializables/IMovementInputChangedEventSubscribable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public interface IMovementInputChangedEventSubscribable
+	{
+		event EventHandler<MovementInputChangedEventArgs> OnMovementInputDataChanged;
+	}
+
+	public sealed class MovementInputChangedEventArgs : EventArgs
+	{
+		//The reason we use float is because of controllers
+		//They may lightly press forward to WALK.
+		//PC players can't really benefit though.
+		public float NewVerticalInput { get; }
+
+		public float NewHorizontalInput { get; }
+
+		/// <inheritdoc />
+		public MovementInputChangedEventArgs(float newVerticalInput, float newHorizontalInput)
+		{
+			NewVerticalInput = newVerticalInput;
+			NewHorizontalInput = newHorizontalInput;
+		}
+	}
+}
