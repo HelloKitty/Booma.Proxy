389f7311569ccc44912c6f46c304ab7755186530
HelloKitty <andrew.blakely@ymail.com>
2019-02-04 18:20:56 -0600

    Added Factory/Destruction interfaces from VRGuardians

---
+++ src/Booma.Proxy.Common/Services/EmptyFactoryContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	public sealed class EmptyFactoryContext
+	{
+		private static EmptyFactoryContext instance;
+		private static object SyncObj = new object();
+
+		public static EmptyFactoryContext Instance
+		{
+			get
+			{
+				if(instance == null)
+				{
+					lock(SyncObj)
+					{
+						if(instance == null)
+						{
+							instance = new EmptyFactoryContext();
+						}
+					}
+				}
+				return instance;
+			}
+		}
+
+		private EmptyFactoryContext()
+		{
+			
+		}
+	}
+}
---
+++ src/Booma.Proxy.Common/Services/IFactoryCreatable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	//TODO: Move this into Glader.Common someday
+	/// <summary>
+	/// Contract for a generic factory.
+	/// </summary>
+	/// <typeparam name="TCreateType">The creation type.</typeparam>
+	/// <typeparam name="TContextType">The context type containing construction data.</typeparam>
+	public interface IFactoryCreatable<out TCreateType, in TContextType>
+	{
+		/// <summary>
+		/// Creates a new instance of the <typeparamref name="TCreateType"/>
+		/// based on the provided <see cref="context"/>.
+		/// </summary>
+		/// <param name="context">The context to construct the instance from.</param>
+		/// <returns>A new instance.</returns>
+		TCreateType Create(TContextType context);
+	}
+}
