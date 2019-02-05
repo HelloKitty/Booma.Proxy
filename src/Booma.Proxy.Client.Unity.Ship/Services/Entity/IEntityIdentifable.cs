8982be8c238b76f6308626aef2cbe7e9448d893c
HelloKitty <andrew.blakely@ymail.com>
2019-02-04 21:35:18 -0600

    Added IEntityIdentifable

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/IEntityIdentifable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Contract for types that are identifable for their entity.
+	/// </summary>
+	public interface IEntityIdentifable
+	{
+		/// <summary>
+		/// The unique identifier for the entity.
+		/// </summary>
+		int EntityGuid { get; }
+	}
+}
