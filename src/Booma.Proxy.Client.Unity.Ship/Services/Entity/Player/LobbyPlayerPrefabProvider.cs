2e4923a2ee6b3320d2f6e7dd3735d4ffbfecac74
HelloKitty <andrew.blakely@ymail.com>
2019-02-05 11:59:27 -0600

    Created simple lobby impl of INetworkPlayerPrefabProvider

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/Player/LobbyPlayerPrefabProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public sealed class LobbyPlayerPrefabProvider : MonoBehaviour, INetworkPlayerPrefabProvider
+	{
+		[SerializeField]
+		private GameObject _localPlayerPrefab;
+
+		[SerializeField]
+		private GameObject _remotePlayerPrefab;
+
+		/// <inheritdoc />
+		public GameObject LocalPlayerPrefab => _localPlayerPrefab;
+
+		/// <inheritdoc />
+		public GameObject RemotePlayerPrefab => _remotePlayerPrefab;
+
+		void Awake()
+		{
+			if(_localPlayerPrefab == null)
+				throw new InvalidOperationException($"Must initialize: {nameof(_localPlayerPrefab)} in {GetType().Name}.");
+
+			if(_remotePlayerPrefab == null)
+				throw new InvalidOperationException($"Must initialize: {nameof(_remotePlayerPrefab)} in {GetType().Name}.");
+		}
+	}
+}
