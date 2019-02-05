677ace296a44ec951e1bb3e55435d4b68e2bb6fe
HelloKitty <andrew.blakely@ymail.com>
2019-02-04 22:52:48 -0600

    Added ILocalPlayerWorldRepresentationSpawnedEventSubscribable

---
+++ src/Booma.Proxy.Client.Unity.Ship/EventInterfaces/ILocalPlayerWorldRepresentationSpawnedEventSubscribable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public interface ILocalPlayerWorldRepresentationSpawnedEventSubscribable
+	{
+		event EventHandler<LocalPlayerWorldObjectSpawnedEventArgs> OnLocalPlayerWorldObjectCreated;
+	}
+
+	public sealed class LocalPlayerWorldObjectSpawnedEventArgs : IEntityIdentifable
+	{
+		public int EntityGuid { get; }
+
+		public GameObject WorldObject { get; }
+
+		/// <inheritdoc />
+		public LocalPlayerWorldObjectSpawnedEventArgs(int entityGuid, [NotNull] GameObject worldObject)
+		{
+			//TODO: Check entity guid validity
+
+			EntityGuid = entityGuid;
+			WorldObject = worldObject ?? throw new ArgumentNullException(nameof(worldObject));
+		}
+	}
+}
