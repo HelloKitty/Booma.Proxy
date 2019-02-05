a23d0e63159aba5b2898ab3adbad2402d93db2ff
HelloKitty <andrew.blakely@ymail.com>
2019-02-04 21:41:25 -0600

    Added simple Pos/Rot model WorldTransform

---
+++ src/Booma.Proxy.Client.Unity.Common/Models/WorldTransform.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public sealed class WorldTransform
+	{
+		public Vector3 Position { get; }
+
+		public Quaternion Rotation { get; }
+
+		/// <inheritdoc />
+		public WorldTransform(Vector3 position, Quaternion rotation)
+		{
+			Position = position;
+			Rotation = rotation;
+		}
+	}
+}
