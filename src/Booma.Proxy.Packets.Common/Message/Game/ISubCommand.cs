533080498879201a4a5ff48ace37db9908f9cd62
HelloKitty <andrew.blakely@ymail.com>
2019-02-04 20:33:15 -0600

    Refactored Subcommand interfaces

---
+++ src/Booma.Proxy.Packets.Common/Message/Game/ISubCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public interface ISubCommand
+	{
+		/// <summary>
+		/// Indicates if the <see cref="CommandSize"/> property is serialized and
+		/// deserialized.
+		/// Child Types can override this to gain access to the single Byte size if needed.
+		/// </summary>
+		bool isSizeSerialized { get; }
+
+		//Since the Type byte is eaten by the polymorphic deserialization process
+		/// <summary>
+		/// The size of the subcommand (subpayload).
+		/// Not needed for deserialization of subcommand.
+		/// </summary>
+		byte CommandSize { get; }
+	}
+}
--- src/Booma.Proxy.Packets.Common/Message/Game/ISubCommand60.cs
+++ src/Booma.Proxy.Packets.Common/Message/Game/ISubCommand60.cs
@@ -6,26 +6,12 @@ using System.Threading.Tasks;
 
 namespace Booma.Proxy
 {
-	public interface ISubCommand60
+	public interface ISubCommand60 : ISubCommand
 	{
 		/// <summary>
 		/// The operation code for the subcommand.
 		/// This is only read for logging of unknown subcommands.
 		/// </summary>
 		SubCommand60OperationCode CommandOperationCode { get; }
-
-		/// <summary>
-		/// Indicates if the <see cref="CommandSize"/> property is serialized and
-		/// deserialized.
-		/// Child Types can override this to gain access to the single Byte size if needed.
-		/// </summary>
-		bool isSizeSerialized { get; }
-
-		//Since the Type byte is eaten by the polymorphic deserialization process
-		/// <summary>
-		/// The size of the subcommand (subpayload).
-		/// Not needed for deserialization of subcommand.
-		/// </summary>
-		byte CommandSize { get; }
 	}
 }
