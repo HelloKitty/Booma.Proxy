861e46337728b38899f47f1265dd8c6b7fc1c525
HelloKitty <andrew.blakely@ymail.com>
2019-02-06 14:46:14 -0600

    Added IRemotePlayerLobbyJoinEventSubscribable

---
+++ src/Booma.Proxy.Client.Unity.Ship/EventInterfaces/IRemotePlayerLobbyJoinEventSubscribable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public interface IRemotePlayerLobbyJoinEventSubscribable
+	{
+		event EventHandler<LobbyJoinedEventArgs> OnRemotePlayerLobbyJoined;
+	}
+}
