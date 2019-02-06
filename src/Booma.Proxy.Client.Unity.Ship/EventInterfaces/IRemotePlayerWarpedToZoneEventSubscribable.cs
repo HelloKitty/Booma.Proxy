e859dd9cb82b1524d2dcb780f30a9ab6f86cc3c5
HelloKitty <andrew.blakely@ymail.com>
2019-02-06 16:18:03 -0600

    Added IRemotePlayerWarpedToZoneEventSubscribable and PlayerWarpedToZoneEventArgs

---
+++ src/Booma.Proxy.Client.Unity.Ship/EventInterfaces/IRemotePlayerWarpedToZoneEventSubscribable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public interface IRemotePlayerWarpedToZoneEventSubscribable
+	{
+		event EventHandler<PlayerWarpedToZoneEventArgs> OnRemotePlayedFinishedWarpToZone;
+	}
+
+	public sealed class PlayerWarpedToZoneEventArgs : EventArgs, IEntityIdentifable
+	{
+		public int EntityGuid { get; }
+
+		public int ZoneId { get; }
+
+		/// <inheritdoc />
+		public PlayerWarpedToZoneEventArgs(int entityGuid, int zoneId)
+		{
+			if(zoneId < 0) throw new ArgumentOutOfRangeException(nameof(zoneId));
+
+			EntityGuid = entityGuid;
+			ZoneId = zoneId;
+		}
+	}
+}
