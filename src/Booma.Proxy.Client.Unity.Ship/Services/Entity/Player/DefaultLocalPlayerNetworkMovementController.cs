1f44dcb2a1263ba51155f3d011d5fbb874780b89
HelloKitty <andrew.blakely@ymail.com>
2019-02-06 12:01:58 -0600

    Created default LocalPlayerNetworkMovementController

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/Player/DefaultLocalPlayerNetworkMovementController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GladNet;
+using SceneJect.Common;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public sealed class DefaultLocalPlayerNetworkMovementController : ILocalPlayerNetworkMovementController
+	{
+		private IPeerPayloadSendService<PSOBBGamePacketPayloadClient> SendService { get; }
+
+		private IUnitScalerStrategy UnitScaler { get; }
+
+		private IZoneSettings ZoneData { get; }
+
+		private IRoomQueryable RoomQueryService { get; }
+
+		private ICharacterSlotSelectedModel PlayerSlotModel { get; }
+
+		/// <inheritdoc />
+		public DefaultLocalPlayerNetworkMovementController([NotNull] IPeerPayloadSendService<PSOBBGamePacketPayloadClient> sendService, [NotNull] IUnitScalerStrategy unitScaler, [NotNull] IZoneSettings zoneData, [NotNull] IRoomQueryable roomQueryService, [NotNull] ICharacterSlotSelectedModel playerSlotModel)
+		{
+			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
+			UnitScaler = unitScaler ?? throw new ArgumentNullException(nameof(unitScaler));
+			ZoneData = zoneData ?? throw new ArgumentNullException(nameof(zoneData));
+			RoomQueryService = roomQueryService ?? throw new ArgumentNullException(nameof(roomQueryService));
+			PlayerSlotModel = playerSlotModel ?? throw new ArgumentNullException(nameof(playerSlotModel));
+		}
+
+		/// <inheritdoc />
+		public Task StopMovementAsync(Vector3 position, Quaternion rotation)
+		{
+			return SendService.SendMessage(new Sub60FinishedMovingCommand(PlayerSlotModel.SlotSelected,
+				UnitScaler.ScaleYRotation(rotation.eulerAngles.y),
+				UnitScaler.UnScale(position).ToNetworkVector3(), RoomQueryService.RoomIdForPlayerById(PlayerSlotModel.SlotSelected), ZoneData.ZoneId).ToPayload());
+		}
+
+		/// <inheritdoc />
+		public Task UpdatedMovementLocation(Vector3 position, Quaternion rotation)
+		{
+			return SendService.SendMessage(new Sub60MovingFastPositionSetCommand(PlayerSlotModel.SlotSelected,
+				UnitScaler.UnScaleYtoZ(position)).ToPayload());
+		}
+	}
+}
