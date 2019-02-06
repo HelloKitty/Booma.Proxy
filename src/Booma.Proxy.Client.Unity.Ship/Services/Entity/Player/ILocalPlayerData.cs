2fc90b48b7dc991cf1f1cefe0fc0e302a9fd9c98
HelloKitty <andrew.blakely@ymail.com>
2019-02-06 16:14:24 -0600

    Added ILocalPlayerData
    Mostly for simplifying common operations over local player data.

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/Player/ILocalPlayerData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public interface ILocalPlayerData
+	{
+		byte SlotIndex { get; }
+
+		int EntityGuid { get; }
+
+		bool isWorldObjectSpawned { get; }
+
+		GameObject WorldObject { get; }
+	}
+}
