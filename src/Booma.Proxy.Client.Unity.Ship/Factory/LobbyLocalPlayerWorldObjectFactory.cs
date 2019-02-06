96d88a12646a9af664c255285dea45f884aa39ea
HelloKitty <andrew.blakely@ymail.com>
2019-02-06 12:11:29 -0600

    Updated local player factory to add it to collections

--- src/Booma.Proxy.Client.Unity.Ship/Factory/LobbyLocalPlayerWorldObjectFactory.cs
+++ src/Booma.Proxy.Client.Unity.Ship/Factory/LobbyLocalPlayerWorldObjectFactory.cs
@@ -16,20 +16,28 @@ namespace Booma.Proxy
 
 		private IWorldObjectToEntityMappable WorldPlayerMap { get; }
 
+		private IEntityGuidMappable<GameObject> EntityGuidToGameObjectMappable { get; }
+
 		private INetworkPlayerPrefabProvider PrefabProvider { get; }
 
 		/// <inheritdoc />
-		public LobbyLocalPlayerWorldObjectFactory([NotNull] IRoomCollection rooms, [NotNull] IWorldObjectToEntityMappable worldPlayerMap, [NotNull] INetworkPlayerPrefabProvider prefabProvider)
+		public LobbyLocalPlayerWorldObjectFactory([NotNull] IRoomCollection rooms, [NotNull] IWorldObjectToEntityMappable worldPlayerMap, [NotNull] INetworkPlayerPrefabProvider prefabProvider, [NotNull] IEntityGuidMappable<GameObject> entityGuidToGameObjectMappable)
 		{
 			Rooms = rooms ?? throw new ArgumentNullException(nameof(rooms));
 			WorldPlayerMap = worldPlayerMap ?? throw new ArgumentNullException(nameof(worldPlayerMap));
 			PrefabProvider = prefabProvider ?? throw new ArgumentNullException(nameof(prefabProvider));
+			EntityGuidToGameObjectMappable = entityGuidToGameObjectMappable ?? throw new ArgumentNullException(nameof(entityGuidToGameObjectMappable));
 		}
 
 		/// <inheritdoc />
 		public GameObject Create(LocalPlayerWorldRepresentationCreationContext context)
 		{
 			GameObject player = GameObject.Instantiate(PrefabProvider.LocalPlayerPrefab, context.SpawnData.AssociatedObject.Position, context.SpawnData.AssociatedObject.Rotation);
+
+			//This makes it so things can go from GameObject (root) to entity guid.
+			WorldPlayerMap.Add(player, context.SpawnData.EntityGuid);
+			//This allows for the reverse
+			EntityGuidToGameObjectMappable.Add(context.SpawnData.EntityGuid, player);
 
 			//TODO: How should we handle rooms?
 			//Rooms.DefaultRoom.TryEnter(player);
