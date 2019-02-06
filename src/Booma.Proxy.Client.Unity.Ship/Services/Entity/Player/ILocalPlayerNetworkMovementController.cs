6bbb62da66e039017dc1a8961af34e6ef06715ad
HelloKitty <andrew.blakely@ymail.com>
2019-02-06 12:01:48 -0600

    Added ILocalPlayerNetworkMovementController

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/Player/ILocalPlayerNetworkMovementController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public interface ILocalPlayerNetworkMovementController
+	{
+		Task StopMovementAsync(Vector3 position, Quaternion rotation);
+
+		Task UpdatedMovementLocation(Vector3 position, Quaternion rotation);
+	}
+}
