3318306cc93a8f9fb6852246b7f7519830b4fbf3
HelloKitty <andrew.blakely@ymail.com>
2019-02-06 16:20:36 -0600

    Added  PlayerZoneData

---
+++ src/Booma.Proxy.Client.Unity.Ship/Models/PlayerZoneData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Data model that contains information about the zone
+	/// a player is in.
+	/// </summary>
+	public sealed class PlayerZoneData
+	{
+		/// <summary>
+		/// The ID of the zone the player is in.
+		/// </summary>
+		public int ZoneId { get; }
+
+		/// <inheritdoc />
+		public PlayerZoneData(int zoneId)
+		{
+			ZoneId = zoneId;
+		}
+	}
+}
