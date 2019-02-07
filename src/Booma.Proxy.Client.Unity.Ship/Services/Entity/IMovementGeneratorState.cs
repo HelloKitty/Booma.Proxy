c48232646f10e4da95cb80c3596bf372c295882a
HelloKitty <andrew.blakely@ymail.com>
2019-02-06 23:53:13 -0600

    Added IMovementGeneratorState
    Mostly just exposes the ability to check finished state and if it's a
    skippable state.

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/IMovementGeneratorState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public interface IMovementGeneratorState : IMovementGenerator<GameObject>
+	{
+		/// <summary>
+		/// Indicates if the movement generation is finished.
+		/// </summary>
+		bool isFinished { get; }
+
+		/// <summary>
+		/// Indicates if the state is skippable.
+		/// AKA think Teleportation (unskippable) vs SlowMovement (skippable).
+		/// Some movement generators have CRITICAL importance like that.
+		/// </summary>
+		bool isSkippable { get; }
+	}
+}
