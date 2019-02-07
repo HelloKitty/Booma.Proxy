c106c09ad96139d6e4ff56cead069da826109aac
HelloKitty <andrew.blakely@ymail.com>
2019-02-06 23:52:30 -0600

    Added IMovementGenerator from VRGuardians

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/IMovementGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma.Proxy
+{
+	public interface IMovementGenerator<in TEntityType> //we make entity type generic so it will be easy to swap between guid/gameobject if needed.
+	{
+		/// <summary>
+		/// Updates the movement for the <see cref="entity"/>
+		/// based on the time <see cref="currentTime"/>.
+		/// </summary>
+		/// <param name="entity"></param>
+		/// <param name="currentTime"></param>
+		void Update(TEntityType entity, long currentTime);
+	}
+}
