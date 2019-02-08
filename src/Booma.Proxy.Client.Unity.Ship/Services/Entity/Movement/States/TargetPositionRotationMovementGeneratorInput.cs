ef762eeb6dc9c360b75aa5903fe88b9ac034e703
HelloKitty <andrew.blakely@ymail.com>
2019-02-07 20:06:43 -0600

    Added TargetPositionRotationMovementGeneratorInput

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/Movement/States/TargetPositionRotationMovementGeneratorInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public sealed class TargetPositionRotationMovementGeneratorInput : DefaultMovementGenerationStateState
+	{
+		/// <summary>
+		/// The final/target rotation desired for movement.
+		/// </summary>
+		public float TargetYAxisRotation { get; }
+
+		/// <inheritdoc />
+		public TargetPositionRotationMovementGeneratorInput(Vector2 targetPosition, float targetYAxisRotation) 
+			: base(targetPosition)
+		{
+			TargetYAxisRotation = targetYAxisRotation;
+		}
+	}
+}
