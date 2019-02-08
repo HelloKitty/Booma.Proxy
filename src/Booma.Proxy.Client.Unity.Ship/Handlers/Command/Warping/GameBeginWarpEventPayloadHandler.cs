f1e8296469172ecc776d21cb39bb3af7531fbcbc
HelloKitty <andrew.blakely@ymail.com>
2019-02-08 12:30:02 -0600

    Recreate Sub62ClientWarpBeginEventCommand handler

---
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Command/Warping/GameBeginWarpEventPayloadHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+using SceneJect.Common;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// The handler for <see cref="Sub62ClientWarpBeginEventCommand"/> which handles this payload
+	/// alerting 
+	/// </summary>
+	[AdditionalRegisterationAs(typeof(IWarpBeginEventSubscribable))]
+	[SceneTypeCreate(GameSceneType.Pioneer2)] //only needed on pioneer2
+	public sealed class GameBeginWarpEventPayloadHandler : Command62Handler<Sub62ClientWarpBeginEventCommand>, IWarpBeginEventSubscribable
+	{
+		/// <inheritdoc />
+		public event EventHandler OnWarpBeginning;
+
+		/// <inheritdoc />
+		public GameBeginWarpEventPayloadHandler(ILog logger)
+			: base(logger)
+		{
+
+		}
+
+		/// <inheritdoc />
+		protected override async Task HandleSubMessage(IPeerMessageContext<PSOBBGamePacketPayloadClient> context, Sub62ClientWarpBeginEventCommand payload)
+		{
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"Recieved: {this.MessageName()} Dispatching: {nameof(IWarpBeginEventSubscribable)}.");
+
+			//TODO: We should really be initializing the quest data, or whatever it is, this packet sends.
+			OnWarpBeginning?.Invoke(this, EventArgs.Empty);
+		}
+	}
+}
