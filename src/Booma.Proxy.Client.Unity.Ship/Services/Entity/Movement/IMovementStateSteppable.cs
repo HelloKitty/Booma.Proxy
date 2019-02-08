c2cee53426357efa51360653504a54cfda282492
HelloKitty <andrew.blakely@ymail.com>
2019-02-07 20:00:06 -0600

    Added IMovementStateSteppable

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/Movement/IMovementStateSteppable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public interface IMovementStateSteppable
+	{
+		/// <summary>
+		/// The current step of the lerp.
+		/// Progress = CurrentStep / LerpDuration.
+		/// </summary>
+		float CurrentStep { get; set; } //public mutable.
+	}
+}
