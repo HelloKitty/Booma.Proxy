9dd326612b45fc28b9a1e183a9632c9f9bb822b3
HelloKitty <andrew.blakely@ymail.com>
2019-02-08 16:29:10 -0600

    Added SubCommand6DOperationCode (Empty)

---
+++ src/Booma.Proxy.Packets.Common/OperationCodes/SubCommand6DOperationCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Enumeration of sub-operation codes for 0x6D packet.
+	/// </summary>
+	public enum SubCommand6DOperationCode : byte //it's sent as byte from server
+	{
+
+	}
+}
