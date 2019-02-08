366088206f66fe8d7348a59d3b3c1f63d55200e3
HelloKitty <andrew.blakely@ymail.com>
2019-02-07 19:59:58 -0600

    Added IMovementStatePositionTargetable

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/Entity/Movement/IMovementStatePositionTargetable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	public interface IMovementStatePositionTargetable
+	{
+		/// <summary>
+		/// The target position for the movement state.
+		/// </summary>
+		Vector2 TargetPosition { get; }
+	}
+}
