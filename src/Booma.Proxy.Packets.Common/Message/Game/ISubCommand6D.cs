3d56e1b0e153d6c8efbbfaa89685d0a272ef3039
HelloKitty <andrew.blakely@ymail.com>
2019-02-08 16:29:21 -0600

    Added ISubCommand6D

---
+++ src/Booma.Proxy.Packets.Common/Message/Game/ISubCommand6D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	public interface ISubCommand6D : ISubCommand<SubCommand6DOperationCode>
+	{
+
+	}
+}
