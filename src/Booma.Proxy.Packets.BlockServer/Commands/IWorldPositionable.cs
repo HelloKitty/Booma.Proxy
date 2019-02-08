552079c8703de2813c0bc510ee4cac39387cad1c
HelloKitty <andrew.blakely@ymail.com>
2019-02-07 19:19:23 -0600

    Added IWorldPositionable<TPositionType>

---
+++ src/Booma.Proxy.Packets.BlockServer/Commands/IWorldPositionable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Contract for commands that have a data component
+	/// related to a world positon.
+	/// </summary>
+	public interface IWorldPositionable<TPositionType>
+	{
+		/// <summary>
+		/// The world position.
+		/// </summary>
+		Vector2<TPositionType> Position { get; }
+	}
+}
