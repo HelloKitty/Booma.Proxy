430eb459795823d8af1e685c6bb79d09c0b16c94
HelloKitty <andrew.blakely@ymail.com>
2019-02-07 19:26:03 -0600

    Implemented non-abstract Fast/Slow command handlers

---
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Command/Movement/FastMoveCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+
+namespace Booma.Proxy
+{
+	[SceneTypeCreate(GameSceneType.LobbyDefault)]
+	public sealed class FastMoveCommandHandler : BaseDefaultPositionChangedEventHandler<Sub60MovingFastPositionSetCommand>
+	{
+		/// <inheritdoc />
+		public FastMoveCommandHandler(IUnitScalerStrategy scaler, ILog logger, IEntityGuidMappable<WorldTransform> worldTransformMappable, IEntityGuidMappable<MovementManager> movementManagerMappable) 
+			: base(scaler, logger, worldTransformMappable, movementManagerMappable)
+		{
+
+		}
+	}
+}
---
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Command/Movement/SlowMoveCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+
+namespace Booma.Proxy
+{
+	[SceneTypeCreate(GameSceneType.LobbyDefault)]
+	public sealed class SlowMoveCommandHandler : BaseDefaultPositionChangedEventHandler<Sub60MovingSlowPositionSetCommand>
+	{
+		/// <inheritdoc />
+		public SlowMoveCommandHandler(IUnitScalerStrategy scaler, ILog logger, IEntityGuidMappable<WorldTransform> worldTransformMappable, IEntityGuidMappable<MovementManager> movementManagerMappable) 
+			: base(scaler, logger, worldTransformMappable, movementManagerMappable)
+		{
+
+		}
+	}
+}
