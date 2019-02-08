4a8dad97e274da54012905d458eb6f02eb365ed3
HelloKitty <andrew.blakely@ymail.com>
2019-02-07 20:52:59 -0600

    Rewrote Sub60FinishedMovingCommand handler as FinishMoveEventhandler

---
+++ src/Booma.Proxy.Client.Unity.Ship/Handlers/Command/Movement/FinishMoveEventhandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+using SceneJect.Common;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	[SceneTypeCreate(GameSceneType.LobbyDefault)]
+	public sealed class FinishMoveEventhandler : BaseDefaultPositionChangedEventHandler<Sub60FinishedMovingCommand>
+	{
+		/// <inheritdoc />
+		public FinishMoveEventhandler(IUnitScalerStrategy scaler, ILog logger, IEntityGuidMappable<WorldTransform> worldTransformMappable, IEntityGuidMappable<MovementManager> movementManagerMappable) 
+			: base(scaler, logger, worldTransformMappable, movementManagerMappable)
+		{
+
+		}
+
+		/// <inheritdoc />
+		protected override IMovementGeneratorState CreateMovementGenerator(Vector2 position, [NotNull] Sub60FinishedMovingCommand command)
+		{
+			if(command == null) throw new ArgumentNullException(nameof(command));
+
+			float yAxisRotation = Quaternion.AngleAxis(Scaler.ScaleYRotation(command.YAxisRotation), Vector3.up).eulerAngles.y;
+
+			return new DefaultMovementGeneratorState(new TargetPositionRotationMovementGeneratorInput(position, yAxisRotation));
+		}
+	}
+}
