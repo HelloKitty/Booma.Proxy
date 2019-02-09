382868676de33a94250adb5fe8f52b8832fbca8f
HelloKitty <andrew.blakely@ymail.com>
2019-02-09 01:43:16 -0600

    Added temp 0x6D 0x70 Sub6DFakePlayerJoinDataNeededCommand

---
+++ src/Booma.Proxy.Packets.BlockServer/Commands/Command6D/Sub6DFakePlayerJoinDataNeededCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	[WireDataContract]
+	[SubCommand6D(SubCommand6DOperationCode.PlayerJoinedData)]
+	public sealed class Sub6DFakePlayerJoinDataNeededCommand : BaseSubCommand6D
+	{
+		//See: https://github.com/justnoxx/psobb-tethealla/blob/master/ship_server/ship_server.c#L11090
+		//This is a lot of different types of data here
+		//the issue is, I don't know how long it's actually suppose to be.
+		[KnownSize(1216)]
+		[WireMember(2)]
+		public byte[] UnknownBytes { get; } = new byte[1216];
+
+		public Sub6DFakePlayerJoinDataNeededCommand()
+		{
+			CommandSize = 1216;
+		}
+	}
+}
