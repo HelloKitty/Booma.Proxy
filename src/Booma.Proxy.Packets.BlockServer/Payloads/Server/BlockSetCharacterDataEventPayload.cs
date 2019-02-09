3baf03bd0517eee50759b70df7a33398701be013
HelloKitty <andrew.blakely@ymail.com>
2019-02-09 16:11:59 -0600

    Added DTO stub for 0x15EA BlockSetCharacterDataEventPayload

---
+++ src/Booma.Proxy.Packets.BlockServer/Payloads/Server/BlockSetCharacterDataEventPayload.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	[WireDataContract]
+	[GameServerPacketPayload(GameNetworkOperationCode.SERVER_CHARACTER_DATA_TYPE)]
+	public sealed class BlockSetCharacterDataEventPayload : PSOBBGamePacketPayloadServer
+	{
+		//TODO: This is a lot of stuff in here. But can't RE it right now. Limited time.
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		public BlockSetCharacterDataEventPayload()
+		{
+			
+		}
+	}
+}
