1cb1bad16eea572fc7672b0d933a6dc845973727
HelloKitty <andrew.blakely@ymail.com>
2019-02-09 02:59:34 -0600

    Fixed the fake 0x70 packet

--- src/Booma.Proxy.Packets.BlockServer/Commands/Command6D/Sub6DFakePlayerJoinDataNeededCommand.cs
+++ src/Booma.Proxy.Packets.BlockServer/Commands/Command6D/Sub6DFakePlayerJoinDataNeededCommand.cs
@@ -14,13 +14,13 @@ namespace Booma.Proxy
 		//See: https://github.com/justnoxx/psobb-tethealla/blob/master/ship_server/ship_server.c#L11090
 		//This is a lot of different types of data here
 		//the issue is, I don't know how long it's actually suppose to be.
-		[KnownSize(1216)]
+		[KnownSize(0x4C0 - 8)]
 		[WireMember(2)]
-		public byte[] UnknownBytes { get; } = new byte[1216];
+		public byte[] UnknownBytes { get; } = new byte[0x4C0 - 8];
 
 		public Sub6DFakePlayerJoinDataNeededCommand()
 		{
-			CommandSize = 1216;
+			CommandSize = 0x4C0;
 		}
 	}
 }
