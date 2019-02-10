ca9d4629d51fa58cad6a7e9bb4afffb55d8812c9
HelloKitty <andrew.blakely@ymail.com>
2019-02-10 16:41:06 -0600

    Added INonTransientEntityDataRemovableEnumerable

---
+++ src/Booma.Proxy.Client.Unity.Common/Collections/INonTransientEntityDataRemovableEnumerable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Just a simplified type of <see cref="IEnumerable{T}"/> where T is <see cref="IEntityCollectionRemovable"/>.
+	/// </summary>
+	public interface INonTransientEntityDataRemovableEnumerable : IEnumerable<IEntityCollectionRemovable>
+	{
+		
+	}
+}
