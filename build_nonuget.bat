c5b7a931a64ea42224424c0fd414a9c1214d2afc
HelloKitty <andrew.blakely@ymail.com>
2019-02-09 23:02:33 -0600

    Fix builscript name

--- build_nonuget.bat
+++ build_nonuget.bat
@@ -2,5 +2,3 @@ dotnet publish src/Booma.Proxy.Client.Unity.BuildAll/Booma.Proxy.Client.Unity.Bu
 
 if not exist "build\client\release" mkdir build\client\release
 xcopy src\Booma.Proxy.Client.Unity.BuildAll\bin\Release\net46\publish build\client\release /Y /q /EXCLUDE:BuildExclude.txt
-
-PAUSE
