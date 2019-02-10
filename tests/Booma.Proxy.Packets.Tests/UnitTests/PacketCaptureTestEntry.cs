fae2cdce69c9aa7de4198feee665a845ad8aa556
HelloKitty <andrew.blakely@ymail.com>
2019-02-10 01:36:17 -0600

    Added better unit test names for other subcommand types

--- tests/Booma.Proxy.Packets.Tests/UnitTests/PacketCaptureTestEntry.cs
+++ tests/Booma.Proxy.Packets.Tests/UnitTests/PacketCaptureTestEntry.cs
@@ -23,11 +23,19 @@ namespace Booma.Proxy
 			/// <inheritdoc />
 			public override string ToString()
 			{
-				//Special naming for 0x60 to make it easier to search
+				//Special naming for subcommand to make it easier to search
 				//We need to support both new captures that automatically have this AND legacy captures which do not
 				if(OpCode == 0x60)
 					if(FileName.ToCharArray().Count(c => c == '_') < 2)
 						return FileName.Replace("0x60_", $"0x60_0x{(int)(BinaryData[6]):X2}_");
+
+				if(OpCode == 0x62)
+					if(FileName.ToCharArray().Count(c => c == '_') < 2)
+						return FileName.Replace("0x62_", $"0x62_0x{(int)(BinaryData[6]):X2}_");
+
+				if(OpCode == 0x6D)
+					if(FileName.ToCharArray().Count(c => c == '_') < 2)
+						return FileName.Replace("0x6D_", $"0x6D_0x{(int)(BinaryData[6]):X2}_");
 
 				return $"{FileName}";
 			}
