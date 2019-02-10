0223adc5685be46385937c892736744a7e0851e9
HelloKitty <andrew.blakely@ymail.com>
2019-02-09 21:56:39 -0600

    Added IBurstingService

---
+++ src/Booma.Proxy.Client.Unity.Ship/Services/IBurstingService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Contract for types that manage the bursting state.
+	/// </summary>
+	public interface IBurstingService : IReadonlyBurstingService
+	{
+		/// <summary>
+		/// Clears the bursting state
+		/// and related bursting data.
+		/// </summary>
+		void ClearBursting();
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="entityGuid"></param>
+		/// <exception cref="InvalidOperationException">Throws if <see cref="isBurstingInProgress"/> is true.</exception>
+		/// <returns>True if the bursting state can be set.</returns>
+		bool SetBurstingEntity(int entityGuid);
+	}
+
+	public interface IReadonlyBurstingService
+	{
+		/// <summary>
+		/// Indicates if a client is currently bursting.
+		/// </summary>
+		bool isBurstingInProgress { get; }
+
+		/// <summary>
+		/// The entity GUID/ID of the bursting player.
+		/// </summary>
+		int? BurstingEntityGuid { get; }
+	}
+}
