002fbd46c4e103b6f463c9362609b27957b28bbc
HelloKitty <andrew.blakely@ymail.com>
2019-12-16 03:30:09 -0600

    Updated build scripts to netstandard2.0

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ dotnet restore Booma.Proxy.sln
 dotnet publish src/Booma.Proxy.Client.Unity.BuildAll/Booma.Proxy.Client.Unity.BuildAll.csproj -c release
 
 if not exist "build\client\release" mkdir build\client\release
-xcopy src\Booma.Proxy.Client.Unity.BuildAll\bin\Release\net46\publish build\client\release /Y /q /EXCLUDE:BuildExclude.txt
+xcopy src\Booma.Proxy.Client.Unity.BuildAll\bin\Release\netstandard2.0\publish build\client\release /Y /q /EXCLUDE:BuildExclude.txt
 
 PAUSE
--- build_nonuget.bat
+++ build_nonuget.bat
@@ -1,4 +1,4 @@
 dotnet publish src/Booma.Proxy.Client.Unity.BuildAll/Booma.Proxy.Client.Unity.BuildAll.csproj -c release
 
 if not exist "build\client\release" mkdir build\client\release
-xcopy src\Booma.Proxy.Client.Unity.BuildAll\bin\Release\net46\publish build\client\release /Y /q /EXCLUDE:BuildExclude.txt
+xcopy src\Booma.Proxy.Client.Unity.BuildAll\bin\Release\netstandard2.0\publish build\client\release /Y /q /EXCLUDE:BuildExclude.txt
