ee501bebe4843eb4ba858e25d02001dc4f50678d
HelloKitty <andrew.blakely@ymail.com>
2019-12-16 22:42:51 -0600

    Added PSOBBGladMMOCompatibilityRegisterationModule

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/IoC/PSOBBGladMMOCompatibilityRegisterationModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using GladMMO;
+using GladNet;
+using SceneJect.Common;
+
+namespace Booma.Proxy
+{
+	public sealed class PSOBBGladMMOCompatibilityRegisterationModule : NonBehaviourDependency
+	{
+		/// <inheritdoc />
+		public override void Register(ContainerBuilder builder)
+		{
+			//This is required to catch outgoing client packets
+			//and convert them to PSOBB packets and send them
+			builder.RegisterType<GladMMONetworkClientPSOBBPayloadIntercepterSendService>()
+				.As<IPeerPayloadSendService<GameClientPacketPayload>>()
+				.SingleInstance();
+		}
+	}
+}
