13cbdc2a879a383f02e6e8d92eb6012cd8d91306
HelloKitty <andrew.blakely@ymail.com>
2019-12-17 23:57:46 -0600

    Added SharedCreateMessageBoxEventPayloadHandler

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Interop/Handlers/Incoming/SharedCreateMessageBoxEventPayloadHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+
+namespace Booma.Proxy
+{
+	[PSOBBHandler]
+	public sealed class SharedCreateMessageBoxEventPayloadHandler : BasePSOBBIncomingInteropPayloadHandler<SharedCreateMessageBoxEventPayload>
+	{
+		/// <inheritdoc />
+		public SharedCreateMessageBoxEventPayloadHandler(ILog logger)
+			: base(logger)
+		{
+
+		}
+
+		public override Task HandleMessage(InteropPSOBBPeerMessageContext context, SharedCreateMessageBoxEventPayload payload)
+		{
+			//We don't yet handle the UI for this so we just log it
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"Recieved MessageBox: {payload.Message}");
+
+			return Task.CompletedTask;
+		}
+	}
+}
