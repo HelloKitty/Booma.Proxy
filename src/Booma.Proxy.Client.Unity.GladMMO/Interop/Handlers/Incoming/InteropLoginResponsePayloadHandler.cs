f27eef0916d779059626f26bf243b418f85a442b
HelloKitty <andrew.blakely@ymail.com>
2019-12-17 22:17:06 -0600

    Added Initial InteropLoginResponsePayloadHandler

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Interop/Handlers/Incoming/InteropLoginResponsePayloadHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+using SceneJect.Common;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Handler that handles the <see cref="SharedLoginResponsePayload"/> and initializes the recieved payload
+	/// information.
+	/// </summary>
+	[PSOBBHandler]
+	public sealed class InteropLoginResponsePayloadHandler : BasePSOBBIncomingInteropPayloadHandler<SharedLoginResponsePayload>
+	{
+		/// <inheritdoc />
+		public InteropLoginResponsePayloadHandler(ILog logger)
+			: base(logger)
+		{
+
+		}
+
+		public override Task HandleMessage(InteropPSOBBPeerMessageContext context, SharedLoginResponsePayload payload)
+		{
+			//We don't yet handle the UI for this so we just log it
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"Received LoginResponse: {payload.ResponseCode}");
+
+			if(Logger.IsDebugEnabled)
+				Logger.Debug($"Tag: {payload.Tag} GuildCard: {payload.GuildCard} TeamId: {payload.TeamId}");
+
+			return Task.CompletedTask;
+		}
+	}
+}
