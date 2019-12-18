aab1abef3b3dd99e5aa4f41f4b9ea09de9206342
HelloKitty <andrew.blakely@ymail.com>
2019-12-17 21:59:20 -0600

    Added GladMMOHandlerAttribute

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Attribute/GladMMOHandlerAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma.Proxy
+{
+	//TODO: Support scene types like Lobby and Game.
+	public sealed class GladMMOHandlerAttribute : Glader.Essentials.SceneTypeCreateAttribute
+	{
+		public GladMMOHandlerAttribute() 
+			: base(1)
+		{
+
+		}
+	}
+}
