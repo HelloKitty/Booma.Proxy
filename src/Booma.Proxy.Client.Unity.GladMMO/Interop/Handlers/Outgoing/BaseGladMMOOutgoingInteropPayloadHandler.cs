7aaa132323719fb762c0c0f243668cf90e54c783
HelloKitty <andrew.blakely@ymail.com>
2019-12-17 20:34:20 -0600

    Added BaseGladMMOOutgoingInteropPayloadHandler

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Interop/Handlers/Outgoing/BaseGladMMOOutgoingInteropPayloadHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladMMO;
+using GladNet;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Base handler for all game handlers.
+	/// </summary>
+	/// <typeparam name="TSpecificPayloadType"></typeparam>
+	public abstract class BaseGladMMOOutgoingInteropPayloadHandler<TSpecificPayloadType> : IPeerMessageHandler<GameClientPacketPayload, PSOBBGamePacketPayloadClient>
+		where TSpecificPayloadType : GameClientPacketPayload
+	{
+		protected ILog Logger { get; }
+
+		/// <inheritdoc />
+		protected BaseGladMMOOutgoingInteropPayloadHandler(ILog logger)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		//TODO: Add exception logging support
+		public abstract Task HandleMessage(IPeerMessageContext<PSOBBGamePacketPayloadClient> context, TSpecificPayloadType payload);
+
+		public bool CanHandle(NetworkIncomingMessage<GameClientPacketPayload> message)
+		{
+			return message.Payload is TSpecificPayloadType;
+		}
+
+		public async Task<bool> TryHandleMessage(IPeerMessageContext<PSOBBGamePacketPayloadClient> context, NetworkIncomingMessage<GameClientPacketPayload> message)
+		{
+			if(!CanHandle(message))
+				return false;
+
+			await HandleMessage(context, (TSpecificPayloadType)message.Payload)
+				.ConfigureAwait(false);
+
+			return true;
+		}
+
+		/// <inheritdoc />
+		public override string ToString()
+		{
+			return $"GameHandler: {GetType().Name} for Payload: {typeof(TSpecificPayloadType).Name}";
+		}
+	}
+}
