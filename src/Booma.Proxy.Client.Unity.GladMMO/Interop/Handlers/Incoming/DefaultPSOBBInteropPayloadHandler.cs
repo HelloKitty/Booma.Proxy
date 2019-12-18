c073bce3883376d6c3898916b610648922441d3e
HelloKitty <andrew.blakely@ymail.com>
2019-12-17 20:34:12 -0600

    Added DefaultPSOBBInteropPayloadHandler

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Interop/Handlers/Incoming/DefaultPSOBBInteropPayloadHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladMMO;
+using GladNet;
+
+namespace Booma.Proxy
+{
+	public sealed class DefaultPSOBBInteropPayloadHandler : IPeerPayloadSpecificMessageHandler<PSOBBGamePacketPayloadServer, PSOBBGamePacketPayloadClient>
+	{
+		private ILog Logger { get; }
+
+		public DefaultPSOBBInteropPayloadHandler([NotNull] ILog logger)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		public async Task HandleMessage(IPeerMessageContext<PSOBBGamePacketPayloadClient> context, PSOBBGamePacketPayloadServer payload)
+		{
+			if(Logger.IsWarnEnabled)
+				Logger.Warn($"PSOBB Unhandled Packet: {payload.OperationCode}:{payload.GetType().Name}");
+		}
+	}
+}
