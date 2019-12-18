2f850d83098787bad71b2970139e761af19e61d1
HelloKitty <andrew.blakely@ymail.com>
2019-12-17 23:57:55 -0600

    Added InteropCharacterInitializationRequestHandler

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Interop/Handlers/Incoming/InteropCharacterInitializationRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+
+namespace Booma.Proxy
+{
+	//TODO: Do we need this on Pioneer2?
+	[PSOBBHandler]
+	public sealed class InteropCharacterInitializationRequestHandler : BasePSOBBIncomingInteropPayloadHandler<BlockCharacterDataInitializationServerRequestPayload>
+	{
+		/// <inheritdoc />
+		public InteropCharacterInitializationRequestHandler(ILog logger)
+			: base(logger)
+		{
+
+		}
+
+		public override Task HandleMessage(InteropPSOBBPeerMessageContext context, BlockCharacterDataInitializationServerRequestPayload payload)
+		{
+			//This is dumb, the server is asking us for character data when it just sent it.
+			//Why? Sega please... But we just send nothing since no SANE developer should trust this data
+			context.PayloadSendService.SendMessage(new BlockCharacterDataInitializeClientResponsePayload());
+
+			return Task.CompletedTask;
+		}
+	}
+}
