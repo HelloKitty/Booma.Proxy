f1e395e91b001aa29583f56b0bac83d3d6c66942
HelloKitty <andrew.blakely@ymail.com>
2019-12-18 00:26:37 -0600

    Added empty InteropLoginJoinEventPayloadHandler

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Interop/Handlers/Incoming/InteropLoginJoinEventPayloadHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladMMO;
+using GladNet;
+
+namespace Booma.Proxy
+{
+	public class InteropLoginJoinEventPayloadHandler : BasePSOBBIncomingInteropPayloadHandler<BlockLobbyJoinEventPayload>
+	{
+		/// <inheritdoc />
+		public BlockLoginJoinEventPayloadHandler(ILog logger)
+			: base(logger)
+		{
+
+		}
+
+		public override async Task HandleMessage(InteropPSOBBPeerMessageContext context, BlockLobbyJoinEventPayload payload)
+		{
+			if(Logger.IsDebugEnabled)
+				Logger.Debug($"**Handling**: {nameof(BlockLobbyJoinEventPayload)}");
+
+			//TODO: Init the slot we were assigned.
+			//Just set the old char slot to the clientid
+			//It's basically like a slot, like a lobby or party slot.
+			//SlotModel.SlotSelected = payload.ClientId;
+
+			//OnLocalPlayerLobbyJoined?.Invoke(this, new LobbyJoinedEventArgs(payload.LobbyNumber, EntityGuid.ComputeEntityGuid(EntityType.Player, payload.ClientId)));
+
+			//Don't send anything here, the server will send a 0x60 0x6F after this
+		}
+	}
+}
