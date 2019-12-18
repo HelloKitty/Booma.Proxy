bb1298abb811882600e338ac712649f5134dee9b
HelloKitty <andrew.blakely@ymail.com>
2019-12-18 01:01:30 -0600

    Added InteropCommand60Handler

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Interop/Handlers/InteropCommand60Handler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Implementation of the message handler
+	/// that handles specifically <see cref="BlockNetworkCommand60EventClientPayload"/> and dispatches the casted <typeparamref name="TSubCommandType"/> command
+	/// to the implementer of this type.
+	/// </summary>
+	/// <typeparam name="TSubCommandType"></typeparam>
+	public abstract class InteropCommand60Handler<TSubCommandType> : SubMessageMessageHandler<TSubCommandType, BlockNetworkCommand60EventServerPayload>
+		where TSubCommandType : BaseSubCommand60
+	{
+		/// <inheritdoc />
+		protected InteropCommand60Handler(ILog logger)
+			: base(logger)
+		{
+
+		}
+
+		/// <inheritdoc />
+		protected override bool CheckIsHandlable(BlockNetworkCommand60EventServerPayload payload)
+		{
+			//Just check if it's the type.
+			return payload.Command is TSubCommandType;
+		}
+
+		/// <inheritdoc />
+		protected override TSubCommandType RetrieveSubMessage(BlockNetworkCommand60EventServerPayload payload)
+		{
+			//If they ask for it just provide it. Could be null, but not up to us.
+			return payload.Command as TSubCommandType;
+		}
+	}
+}
