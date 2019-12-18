f8366b69ac78087020e7b316bf6cccc99c8ba187
HelloKitty <andrew.blakely@ymail.com>
2019-12-17 20:34:52 -0600

    Refactored GladMMOManagedClientPSOBBInterceptor

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Interop/GladMMOManagedClientPSOBBInterceptor.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladMMO;
+using GladNet;
+using Nito.AsyncEx;
+
+namespace Booma.Proxy
+{
+	public sealed class GladMMOManagedClientPSOBBInterceptor : IManagedNetworkClient<GameClientPacketPayload, GameServerPacketPayload>, IPeerRequestSendService<PSOBBGamePacketPayloadClient>
+	{
+		private ILog Logger { get; }
+
+		private IManagedNetworkClient<PSOBBGamePacketPayloadClient, PSOBBGamePacketPayloadServer> PSOBBNetworkClient { get; }
+
+		private MessageHandlerService<PSOBBGamePacketPayloadServer, PSOBBGamePacketPayloadClient> PSOBBIncomingMessageHandlers { get; }
+
+		private MessageHandlerService<GameClientPacketPayload, PSOBBGamePacketPayloadClient> GladMMOOutgoingMessageHandlers { get; }
+
+		public GladMMOManagedClientPSOBBInterceptor([NotNull] ILog logger, [NotNull] IManagedNetworkClient<PSOBBGamePacketPayloadClient, PSOBBGamePacketPayloadServer> psobbNetworkClient, 
+			MessageHandlerService<PSOBBGamePacketPayloadServer, PSOBBGamePacketPayloadClient> psobbIncomingMessageHandlers, 
+			MessageHandlerService<GameClientPacketPayload, PSOBBGamePacketPayloadClient> gladMmoOutgoingMessageHandlers)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			PSOBBNetworkClient = psobbNetworkClient ?? throw new ArgumentNullException(nameof(psobbNetworkClient));
+			PSOBBIncomingMessageHandlers = psobbIncomingMessageHandlers;
+			GladMMOOutgoingMessageHandlers = gladMmoOutgoingMessageHandlers;
+		}
+
+		async Task<SendResult> IPeerPayloadSendService<GameClientPacketPayload>.SendMessage<TPayloadType>(TPayloadType payload, DeliveryMethod method = DeliveryMethod.ReliableOrdered)
+		{
+			if(Logger.IsDebugEnabled)
+				Logger.Debug($"GladMMO PSOBB payload intercepted: {payload.GetType().Name}");
+
+			await GladMMOOutgoingMessageHandlers.TryHandleMessage(new DefaultPeerMessageContext<PSOBBGamePacketPayloadClient>(PSOBBNetworkClient, PSOBBNetworkClient, this), new NetworkIncomingMessage<GameClientPacketPayload>(new HeaderlessPacketHeader(1), payload))
+				.ConfigureAwait(false);
+
+			return SendResult.Sent;
+		}
+
+		//TODO: Implement SendImmediately mechanics.
+		async Task<SendResult> IPeerPayloadSendService<GameClientPacketPayload>.SendMessageImmediately<TPayloadType>(TPayloadType payload, DeliveryMethod method = DeliveryMethod.ReliableOrdered)
+		{
+			if(Logger.IsDebugEnabled)
+				Logger.Debug($"GladMMO PSOBB payload intercepted: {payload.GetType().Name}");
+
+			await GladMMOOutgoingMessageHandlers.TryHandleMessage(new DefaultPeerMessageContext<PSOBBGamePacketPayloadClient>(PSOBBNetworkClient, PSOBBNetworkClient, this), new NetworkIncomingMessage<GameClientPacketPayload>(new HeaderlessPacketHeader(1), payload))
+				.ConfigureAwait(false);
+
+			return SendResult.Sent;
+		}
+
+		public Task DisconnectAsync(int delay)
+		{
+			Logger.Debug($"GladMMO disconnect request.");
+			return PSOBBNetworkClient.DisconnectAsync(delay);
+		}
+
+		public Task<bool> ConnectAsync(string ip, int port)
+		{
+			Logger.Debug($"GladMMO Connect request: {ip}:{port}");
+
+			//TODO: Hack to connect locally.
+			return PSOBBNetworkClient.ConnectAsync("192.168.0.12", 5003);
+		}
+
+		//It's important for handling restarting that
+		//we indicate accurate connection state
+		//when mocking.
+		public bool isConnected => PSOBBNetworkClient.isConnected;
+
+		public async Task<NetworkIncomingMessage<GameServerPacketPayload>> ReadMessageAsync(CancellationToken token)
+		{
+			while (isConnected && !token.IsCancellationRequested)
+			{
+				NetworkIncomingMessage<PSOBBGamePacketPayloadServer> message = await PSOBBNetworkClient.ReadMessageAsync(token)
+					.ConfigureAwait(false);
+
+				Logger.Debug($"Recieved PSOBB packet: {message.Payload.OperationCode}:{message.Payload.GetType().Name}");
+
+				await PSOBBIncomingMessageHandlers.TryHandleMessage(new DefaultPeerMessageContext<PSOBBGamePacketPayloadClient>(PSOBBNetworkClient, PSOBBNetworkClient, this), message)
+					.ConfigureAwait(false);
+			}
+
+			//We don't 1:1 transform packets, that would be too difficult
+			//so the above handler API will
+			return null;
+		}
+
+		public async Task<TResponseType> InterceptPayload<TResponseType>(CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException("TODO: Implement this stupid stuff.");
+		}
+
+		public async Task WriteAsync(byte[] bytes, int offset, int count)
+		{
+			throw new NotImplementedException("TODO: Implement this stupid stuff.");
+		}
+
+		public async Task<TResponseType> SendRequestAsync<TResponseType>(PSOBBGamePacketPayloadClient request, DeliveryMethod method = DeliveryMethod.ReliableOrdered, CancellationToken cancellationToken = new CancellationToken())
+		{
+			throw new NotImplementedException($"Async message sending is not implemented.");
+		}
+	}
+}
