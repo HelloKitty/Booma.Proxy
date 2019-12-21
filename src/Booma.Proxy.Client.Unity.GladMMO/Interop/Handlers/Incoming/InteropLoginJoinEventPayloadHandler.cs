ee6957de8f2f4165cb75b20cf5ff0bab7851eb47
HelloKitty <andrew.blakely@ymail.com>
2019-12-21 17:44:21 -0600

    Added Guid mapping to 0x67 handling

--- src/Booma.Proxy.Client.Unity.GladMMO/Interop/Handlers/Incoming/InteropLoginJoinEventPayloadHandler.cs
+++ src/Booma.Proxy.Client.Unity.GladMMO/Interop/Handlers/Incoming/InteropLoginJoinEventPayloadHandler.cs
@@ -13,11 +13,15 @@ namespace Booma.Proxy
 	{
 		private ICharacterSlotSelectedModel SlotModel { get; }
 
+		private IInteropEntityMappable PsoEntityKeyToGuidMappable { get; }
+
 		/// <inheritdoc />
-		public InteropLoginJoinEventPayloadHandler(ILog logger, [NotNull] ICharacterSlotSelectedModel slotModel)
+		public InteropLoginJoinEventPayloadHandler(ILog logger, [NotNull] ICharacterSlotSelectedModel slotModel,
+			[NotNull] IInteropEntityMappable psoEntityKeyToGuidMappable)
 			: base(logger)
 		{
 			SlotModel = slotModel ?? throw new ArgumentNullException(nameof(slotModel));
+			PsoEntityKeyToGuidMappable = psoEntityKeyToGuidMappable ?? throw new ArgumentNullException(nameof(psoEntityKeyToGuidMappable));
 		}
 
 		public override async Task HandleMessage(InteropPSOBBPeerMessageContext context, BlockLobbyJoinEventPayload payload)
@@ -29,6 +33,21 @@ namespace Booma.Proxy
 			//Just set the old char slot to the clientid
 			//It's basically like a slot, like a lobby or party slot.
 			SlotModel.SlotSelected = payload.ClientId;
+
+			foreach (var entry in payload.LobbyCharacterData)
+			{
+				int entityGuid = EntityGuid.ComputeEntityGuid(EntityType.Player, (short)entry.PlayerHeader.ClientId);
+
+				//TODO: Support guids larger than shorts.
+				NetworkEntityGuid networkEntityGuid = NetworkEntityGuidBuilder.New()
+					.WithType(GladMMO.EntityType.Player)
+					.WithId((int)(0xFFFF & entry.PlayerHeader.GuildCardNumber))
+					.Build();
+
+				Logger.Debug($"RemotePlayer: {entry.PlayerHeader.CharacterName} GuildCard: {entry.PlayerHeader.GuildCardNumber} Slot: {entry.PlayerHeader.ClientId} PSOGuid: {entityGuid} GladMMOGUID: {networkEntityGuid.ToString()}");
+
+				PsoEntityKeyToGuidMappable[entityGuid] = networkEntityGuid;
+			}
 
 			//OnLocalPlayerLobbyJoined?.Invoke(this, new LobbyJoinedEventArgs(payload.LobbyNumber, EntityGuid.ComputeEntityGuid(EntityType.Player, payload.ClientId)));
 
