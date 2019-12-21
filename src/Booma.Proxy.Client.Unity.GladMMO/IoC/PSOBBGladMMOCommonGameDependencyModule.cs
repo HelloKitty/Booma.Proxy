8c0a0548a9775dd4e1864cf5eda5d71cc8a85fc0
HelloKitty <andrew.blakely@ymail.com>
2019-12-21 12:34:26 -0600

    Added PSO entity component system

--- src/Booma.Proxy.Client.Unity.GladMMO/IoC/PSOBBGladMMOCommonGameDependencyModule.cs
+++ src/Booma.Proxy.Client.Unity.GladMMO/IoC/PSOBBGladMMOCommonGameDependencyModule.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using Autofac;
+using Autofac.Core;
+using Glader.Essentials;
+using GladMMO;
 
 namespace Booma.Proxy
 {
@@ -20,6 +23,33 @@ namespace Booma.Proxy
 			//TransientInteropEntityMappable : IInteropEntityMappable
 			builder.RegisterType<TransientInteropEntityMappable>()
 				.As<IInteropEntityMappable>()
+				.SingleInstance();
+
+			RegisterComponentSystem(builder);
+		}
+
+		private void RegisterComponentSystem(ContainerBuilder builder)
+		{
+			//HelloKitty: We actually have to do this manually, and not use GladerEssentials because we use simplified interfaces.
+			//The below is kinda a hack to register the non-generic types in the
+			//removabale collection
+			List<IEntityCollectionRemovable<int>> removableComponentsList = new List<IEntityCollectionRemovable<int>>(10);
+
+			builder.RegisterGeneric(typeof(EntityGuidDictionary<,>))
+				.AsSelf()
+				.As(typeof(IReadonlyEntityGuidMappable<,>))
+				.As(typeof(IEntityGuidMappable<,>))
+				.OnActivated(args =>
+				{
+					if(args.Instance is IEntityCollectionRemovable<int> removable)
+						removableComponentsList.Add(removable);
+				})
+				.SingleInstance();
+
+			//This will allow everyone to register the removable collection collection.
+			builder.RegisterInstance(removableComponentsList)
+				.As<IReadOnlyCollection<IEntityCollectionRemovable<int>>>()
+				.AsSelf()
 				.SingleInstance();
 		}
 	}
