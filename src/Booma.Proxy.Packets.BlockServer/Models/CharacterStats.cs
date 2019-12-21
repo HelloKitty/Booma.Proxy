a6437669cea17079777b15fd955c2153100d8aee
HelloKitty <andrew.blakely@ymail.com>
2019-12-21 13:25:57 -0600

    Added Serializable CharacterStats

---
+++ src/Booma.Proxy.Packets.BlockServer/Models/CharacterStats.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	[WireDataContract]
+	public sealed class CharacterStats
+	{
+		/*  uint16_t atp;
+		    uint16_t mst;
+		    uint16_t evp;
+		    uint16_t hp;
+		    uint16_t dfp;
+		    uint16_t ata;
+		    uint16_t lck;*/
+
+		//TODO: Provide a better API for accessing the stats data.
+		[KnownSize(7)]
+		[WireMember(1)]
+		public ushort[] Stats { get; }
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private CharacterStats()
+		{
+			
+		}
+	}
+}
