b93379a57ca615ad9e135954ad7ab616d809a74c
HelloKitty <andrew.blakely@ymail.com>
2019-12-21 21:03:19 -0600

    Added UnknownZone handling to ZoneAreaWarped event

--- src/Booma.Proxy.Client.Unity.GladMMO/Engine/Events/IPlayerZoneAreaWarpedEventSubscribable.cs
+++ src/Booma.Proxy.Client.Unity.GladMMO/Engine/Events/IPlayerZoneAreaWarpedEventSubscribable.cs
@@ -15,6 +15,9 @@ namespace Booma.Proxy
 	{
 		public byte Identifier { get; }
 
+		/// <summary>
+		/// If -1 it means they never had a previous zone.
+		/// </summary>
 		public int OldZoneId { get; }
 
 		public int NewZoneId { get; }
@@ -22,11 +25,20 @@ namespace Booma.Proxy
 		public PlayerZoneAreaWarpedEventArgs(byte identifier, int oldZoneId, int newZoneId)
 		{
 			if (identifier <= 0) throw new ArgumentOutOfRangeException(nameof(identifier));
-			if (oldZoneId <= 0) throw new ArgumentOutOfRangeException(nameof(oldZoneId));
 			if (newZoneId <= 0) throw new ArgumentOutOfRangeException(nameof(newZoneId));
 
 			Identifier = identifier;
 			OldZoneId = oldZoneId;
+			NewZoneId = newZoneId;
+		}
+
+		public PlayerZoneAreaWarpedEventArgs(byte identifier, int newZoneId)
+			: this(identifier, -1, newZoneId)
+		{
+			if(identifier <= 0) throw new ArgumentOutOfRangeException(nameof(identifier));
+			if(newZoneId <= 0) throw new ArgumentOutOfRangeException(nameof(newZoneId));
+
+			Identifier = identifier;
 			NewZoneId = newZoneId;
 		}
 	}
