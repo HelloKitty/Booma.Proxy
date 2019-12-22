7900e26bc3524a614f843d1dc172c34db98d90bc
HelloKitty <andrew.blakely@ymail.com>
2019-12-21 20:45:55 -0600

    Added IPlayerZoneAreaWarpedEventSubscribable

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Engine/Events/IPlayerZoneAreaWarpedEventSubscribable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GladMMO;
+
+namespace Booma.Proxy
+{
+	public interface IPlayerZoneAreaWarpedEventSubscribable
+	{
+		event EventHandler<PlayerZoneAreaWarpedEventArgs> OnPlayerZoneAreWarped;
+	}
+
+
+	public class PlayerZoneAreaWarpedEventArgs : EventArgs, Booma.Proxy.IMessageContextIdentifiable
+	{
+		public byte Identifier { get; }
+
+		public int OldZoneId { get; }
+
+		public int NewZoneId { get; }
+
+		public PlayerZoneAreaWarpedEventArgs(byte identifier, int oldZoneId, int newZoneId)
+		{
+			if (identifier <= 0) throw new ArgumentOutOfRangeException(nameof(identifier));
+			if (oldZoneId <= 0) throw new ArgumentOutOfRangeException(nameof(oldZoneId));
+			if (newZoneId <= 0) throw new ArgumentOutOfRangeException(nameof(newZoneId));
+
+			Identifier = identifier;
+			OldZoneId = oldZoneId;
+			NewZoneId = newZoneId;
+		}
+	}
+}
