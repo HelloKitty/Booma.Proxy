2e06f0f7a19370cfd966e0aac0f435725ef1c771
HelloKitty <andrew.blakely@ymail.com>
2019-12-22 22:21:37 -0600

    Added movement command handlers

---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Interop/Handlers/Incoming/Command/Movement/FinishMoveEventhandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Glader.Essentials;
+using GladMMO;
+using GladNet;
+using SceneJect.Common;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	[PSOBBHandler]
+	public sealed class FinishMoveEventhandler : BaseInteropDefaultPositionChangedEventHandler<Sub60FinishedMovingCommand>
+	{
+		public FinishMoveEventhandler(IUnitScalerStrategy scaler,
+			ILog logger,
+			IEntityGuidMappable<int, GladMMO.WorldTransform> worldTransformMappable,
+			IInteropEntityMappable guidMappable,
+			GladMMO.IEntityGuidMappable<GladMMO.WorldTransform> gladMmoWorldTransformMappable)
+			: base(scaler, logger, worldTransformMappable, guidMappable, gladMmoWorldTransformMappable)
+		{
+
+		}
+
+		protected override Vector2 ComputeMovementDirection(Vector2 position, Vector2 lastPosition)
+		{
+			return Vector2.zero;
+		}
+
+		protected override float GetYAxisPosition(NetworkEntityGuid networtkEntityGuid, Sub60FinishedMovingCommand command)
+		{
+			return Scaler.ScaleY(command.Position.Y);
+		}
+	}
+}
---
+++ src/Booma.Proxy.Client.Unity.GladMMO/Interop/Handlers/Incoming/Command/Movement/SlowMoveCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Glader.Essentials;
+
+namespace Booma.Proxy
+{
+	[PSOBBHandler]
+	public sealed class SlowMoveCommandHandler : BaseInteropDefaultPositionChangedEventHandler<Sub60MovingSlowPositionSetCommand>
+	{
+		public SlowMoveCommandHandler(IUnitScalerStrategy scaler,
+			ILog logger,
+			IEntityGuidMappable<int, GladMMO.WorldTransform> worldTransformMappable,
+			IInteropEntityMappable guidMappable,
+			GladMMO.IEntityGuidMappable<GladMMO.WorldTransform> gladMmoWorldTransformMappable)
+			: base(scaler, logger, worldTransformMappable, guidMappable, gladMmoWorldTransformMappable)
+		{
+
+		}
+	}
+}
