253a9f3d0b5249c81387269dde8e05342cdfe45c
HelloKitty <andrew.blakely@ymail.com>
2020-12-21 16:13:48 -0600

    Moved Booma.Proxy.Common to API.Common

---
+++ src/Booma.API.Common/Entity/IEntityGuidMappable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma.Proxy
+{
+	public interface IEntityGuidMappable<TValue> : IDictionary<int, TValue>, IEntityCollectionRemovable
+	{
+		
+	}
+}
---
+++ src/Booma.API.Common/Math/Vector2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Generic 2-dimensional vector.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	[Serializable]
+	[WireDataContract]
+	public class Vector2<T>
+	{
+		/// <summary>
+		/// X value.
+		/// </summary>
+		[WireMember(1)]
+		public T X { get; }
+
+		/// <summary>
+		/// Y value.
+		/// </summary>
+		[WireMember(2)]
+		public T Y { get; }
+
+		/// <inheritdoc />
+		public Vector2(T x, T y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		protected Vector2()
+		{
+			
+		}
+
+		/// <inheritdoc />
+		public override string ToString()
+		{
+			return $"X: {X} Y: {Y}";
+		}
+	}
+}
