7d43c8e5c92b36e1dbdd913dd6f4a3052874efa0
HelloKitty <andrew.blakely@ymail.com>
2020-12-22 00:10:30 -0600

    Fixed 0xA0 SharedShipListEventPayload serialization

--- src/Booma.Packet.Game/Shared/Payloads/Server/SharedShipListEventPayload.cs
+++ src/Booma.Packet.Game/Shared/Payloads/Server/SharedShipListEventPayload.cs
@@ -20,21 +20,31 @@ namespace Booma.Proxy
 		public override bool isFlagsSerialized { get; } = false;
 
 		//PSOBB sends 4 byte Flags with the entry count. We disable Flags though to steal the 4 bytes
-		[SendSize(SendSizeAttribute.SizeType.Int32, 1)] //for some reason they send 1 less than the actual size 
+		[SendSize(SendSizeAttribute.SizeType.Int32)] //for some reason they send 1 less than the actual size 
 		[WireMember(1)]
 		private MenuListing[] _MenuListings { get; } //settable for removing the garbage entry
 
 		/// <summary>
 		/// All the menu listings sent in the packet.
 		/// </summary>
-		public IEnumerable<MenuListing> MenuListings => _MenuListings;
+		public IEnumerable<MenuListing> MenuListings => EnumerateListings();
+
+		private IEnumerable<MenuListing> EnumerateListings()
+		{
+			foreach(var entry in _MenuListings)
+				yield return entry;
+
+			yield return LastMenuListing;
+		}
 
 		/// <summary>
 		/// Only the ship menu listings sent in the packet.
 		/// </summary>
-		public IEnumerable<MenuListing> Ships => _MenuListings.Skip(1);
+		public IEnumerable<MenuListing> Ships => MenuListings.Skip(1);
 
 		//TODO: Failing test cases for mismatch size. The reason it is happening is public Teth sends 8 extra padding bytes that it doesn't need.
+		[WireMember(2)]
+		private MenuListing LastMenuListing { get; set; }
 
 		//Serializer ctor
 		private SharedShipListEventPayload()
