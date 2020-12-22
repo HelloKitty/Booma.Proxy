6777785a30e0e789db85014d79379c63bcb0387f
HelloKitty <andrew.blakely@ymail.com>
2020-12-21 18:17:17 -0600

    Moved Unit scaling to common API

---
+++ src/Booma.API.Common/Math/Scaling/World/DefaultPSOUnitScalerStrategy.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma.Proxy
+{
+	public class DefaultPSOUnitScalerStrategy : IUnitScalerStrategy
+	{
+		/// <summary>
+		/// The unit scale to scale to and from.
+		/// </summary>
+		private Vector3<float> Scale { get; set; }
+
+		private DefaultPSOUnitScalerStrategy(Vector3<float> scale)
+		{
+			//Can't call methods in struct ctor.
+			bool isValid = scale.X != 0 && scale.Y != 0 && scale.Z != 0;
+
+			if(!isValid)
+				throw new ArgumentException($"Provided vector {nameof(scale)} must not have any 0 components.");
+
+			Scale = scale;
+		}
+
+		public DefaultPSOUnitScalerStrategy()
+			: this(new Vector3<float>(0.2f, 0.2f, -0.2f))
+		{
+			
+		}
+
+		/// <summary>
+		/// Verifies that the scale isn't 0 for any component.
+		/// </summary>
+		/// <param name="scale">The scale to check.</param>
+		/// <returns>True if the scale is valid.</returns>
+		private bool VerifyScale(Vector3<float> scale)
+		{
+			return scale.X != 0 && scale.Y != 0 && scale.Z != 0;
+		}
+
+		/// <inheritdoc />
+		public float ScaleX(float x)
+		{
+			return Scale.X * x;
+		}
+
+		/// <inheritdoc />
+		public float ScaleY(float y)
+		{
+			return Scale.Y * y;
+		}
+
+		/// <inheritdoc />
+		public float ScaleZ(float z)
+		{
+			return Scale.Z * z;
+		}
+
+		/// <inheritdoc />
+		public float UnScaleX(float x)
+		{
+			return x / Scale.X;
+		}
+
+		/// <inheritdoc />
+		public float UnScaleY(float y)
+		{
+			return y / Scale.Y;
+		}
+
+		/// <inheritdoc />
+		public float UnScaleZ(float z)
+		{
+			return z / Scale.Z;
+		}
+	}
+}
---
+++ src/Booma.API.Common/Math/Scaling/World/UnitScalerStrategyExtensions.cs
@@ -0,0 +1,140 @@
+using System;
+
+namespace Booma.Proxy
+{
+	public static class UnitScalerStrategyExtensions
+	{
+		/// <summary>
+		/// Fully scales the <see cref="Vector3"/> by the scaling.
+		/// </summary>
+		/// <param name="scaler">The scaling service.</param>
+		/// <param name="vector">The vector to scale.</param>
+		/// <returns>A new scaled vector.</returns>
+		public static Vector3<float> Scale(this IUnitScalerStrategy scaler, Vector3<float> vector)
+		{
+			return new Vector3<float>(scaler.ScaleX(vector.X), scaler.ScaleY(vector.Y), scaler.ScaleZ(vector.Z));
+		}
+
+		/// <summary>
+		/// Fully scales the <see cref="Vector2"/> by the scaling.
+		/// </summary>
+		/// <param name="scaler">The scaling service.</param>
+		/// <param name="vector">The vector to scale.</param>
+		/// <returns>A new scaled vector.</returns>
+		public static Vector2<float> Scale(this IUnitScalerStrategy scaler, Vector2<float> vector)
+		{
+			return new Vector2<float>(scaler.ScaleX(vector.X), scaler.ScaleY(vector.Y));
+		}
+
+		/// <summary>
+		/// Fully scales the <see cref="Vector2"/> by the scaling.
+		/// </summary>
+		/// <param name="scaler">The scaling service.</param>
+		/// <param name="vector">The vector to scale.</param>
+		/// <returns>A new scaled vector.</returns>
+		public static Vector2<float> ScaleYasZ(this IUnitScalerStrategy scaler, Vector2<float> vector)
+		{
+			return new Vector2<float>(scaler.ScaleX(vector.X), scaler.ScaleZ(vector.Y));
+		}
+
+		/// <summary>
+		/// Scales the <see cref="Vector3"/> by the scaling.
+		/// Will use the vector2's Y component as the Vector3's Z component.
+		/// Will use the Y component of the original vector.
+		/// </summary>
+		/// <param name="scaler">The scaling service.</param>
+		/// <param name="vector">The vector to scale.</param>
+		/// <returns>A new scaled vector.</returns>
+		public static Vector3<float> ScaleYtoZ(this IUnitScalerStrategy scaler, Vector3<float> vector)
+		{
+			return new Vector3<float>(scaler.ScaleX(vector.X), vector.Y, scaler.ScaleZ(vector.Z));
+		}
+
+		/// <summary>
+		/// Scales the <see cref="Vector3"/> by the scaling with the option to ignore some dimensions flags.
+		/// </summary>
+		/// <param name="scaler">The scaling service.</param>
+		/// <param name="vector">The vector to scale.</param>
+		/// <param name="ignoreFlags">Which dimensions to ignore.</param>
+		/// <returns>A new scaled vector.</returns>
+		public static Vector3<float> Scale(this IUnitScalerStrategy scaler, Vector3<float> vector, UnitScaleFlags ignoreFlags)
+		{
+			float x = ignoreFlags.QuickFlagsCheck(UnitScaleFlags.X) ? vector.X : scaler.ScaleX(vector.X);
+			float y = ignoreFlags.QuickFlagsCheck(UnitScaleFlags.Y) ? vector.Y : scaler.ScaleY(vector.Y); ;
+			float z = ignoreFlags.QuickFlagsCheck(UnitScaleFlags.Z) ? vector.Z : scaler.ScaleZ(vector.Z); ;
+
+			return new Vector3<float>(x, y, z);
+		}
+
+		/// <summary>
+		/// Fully unscales the <see cref="Vector3"/> using the scaling service
+		/// </summary>
+		/// <param name="scaler">The scaling service.</param>
+		/// <param name="vector">The vector to scale.</param>
+		/// <returns>A new scaled vector.</returns>
+		public static Vector3<float> UnScale(this IUnitScalerStrategy scaler, Vector3<float> vector)
+		{
+			return new Vector3<float>(scaler.UnScaleX(vector.X), scaler.UnScaleY(vector.Y), scaler.UnScaleZ(vector.Z));
+		}
+
+		/// <summary>
+		/// Unscales the <see cref="Vector3"/> by the unscaling with the option to ignore some dimensions flags.
+		/// </summary>
+		/// <param name="scaler">The scaling service.</param>
+		/// <param name="vector">The vector to scale.</param>
+		/// <param name="ignoreFlags">Which dimensions to ignore.</param>
+		/// <returns>A new scaled vector.</returns>
+		public static Vector3<float> UnScale(this IUnitScalerStrategy scaler, Vector3<float> vector, UnitScaleFlags ignoreFlags)
+		{
+			float x = ignoreFlags.QuickFlagsCheck(UnitScaleFlags.X) ? vector.X : scaler.UnScaleX(vector.X);
+			float y = ignoreFlags.QuickFlagsCheck(UnitScaleFlags.Y) ? vector.Y : scaler.UnScaleY(vector.Y); ;
+			float z = ignoreFlags.QuickFlagsCheck(UnitScaleFlags.Z) ? vector.Z : scaler.UnScaleZ(vector.Z); ;
+
+			return new Vector3<float>(x, y, z);
+		}
+
+		/// <summary>
+		/// Unsclaes the <see cref="Vector3"/> by the scaling.
+		/// Maps the Z component of the <see cref="Vector3"/> to the Y component of the
+		/// <see cref="Vector2{T}"/>.
+		/// </summary>
+		/// <param name="scaler">The scaling service.</param>
+		/// <param name="vector">The vector to scale.</param>
+		/// <returns>A new scaled vector.</returns>
+		public static Vector2<float> UnScaleYtoZ(this IUnitScalerStrategy scaler, Vector3<float> vector)
+		{
+			return new Vector2<float>(scaler.UnScaleX(vector.X), scaler.UnScaleZ(vector.Z));
+		}
+
+		/// <summary>
+		/// Scales the Y rotation based on the unit scale.
+		/// </summary>
+		/// <param name="scaler">The scaler.</param>
+		/// <param name="rotation">The rotation to scale.</param>
+		/// <returns></returns>
+		public static float ScaleYRotation(this IUnitScalerStrategy scaler, float rotation)
+		{
+			//TODO: IS this right?
+			//This is odd but if we have both Z and X flipped then the Y will be the same.
+			return (Math.Sign(scaler.ScaleZ(1)) + Math.Sign(scaler.ScaleX(1))) <= 0.0f ? -rotation + 180f : rotation;
+		}
+
+		/// <summary>
+		/// Unscales the Y rotation based on the unit scale.
+		/// </summary>
+		/// <param name="scaler">The scaler.</param>
+		/// <param name="rotation">The rotation to scale.</param>
+		/// <returns></returns>
+		public static float UnScaleYRotation(this IUnitScalerStrategy scaler, float rotation)
+		{
+			//TODO: IS this right?
+			//This is odd but if we have both Z and X flipped then the Y will be the same.
+			return (Math.Sign(scaler.ScaleZ(1)) + Math.Sign(scaler.ScaleX(1))) <= 0.0f ? -rotation : rotation;
+		}
+
+		public static bool QuickFlagsCheck(this UnitScaleFlags flags, UnitScaleFlags flag)
+		{
+			return (flags & flag) != 0;
+		}
+	}
+}
