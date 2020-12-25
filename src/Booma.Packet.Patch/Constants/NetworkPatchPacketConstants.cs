cac224e7a0761e8be7f287be9fb23033071b8625
HelloKitty <andrew.blakely@ymail.com>
2020-12-25 00:54:20 -0600

    Added NetworkPatchPacketConstants

---
+++ src/Booma.Packet.Patch/Constants/NetworkPatchPacketConstants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	/// <summary>
+	/// Static constants Type for patch packets.
+	/// </summary>
+	public static class NetworkPatchPacketConstants
+	{
+		/// <summary>
+		/// Patch packet headers are 2 byte Uint16 values of packet length.
+		/// </summary>
+		public const int PATCH_PACKET_HEADER_SIZE = 2;
+
+		/// <summary>
+		/// Patch packet's maximum size allowed.
+		/// </summary>
+		public const int PATCH_PACKET_MAXIMUM_SIZE = 1024;
+	}
+}
