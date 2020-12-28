b6f2cb64bc31cdc7939c87a16bd948acfc360f31
HelloKitty <andrew.blakely@ymail.com>
2020-12-27 22:20:55 -0600

    Added client 0xE2 packet

---
+++ src/Booma.Packet.Game/Character/Payloads/Client/CharacterOptionsUpdateRequestPayload.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace Booma.Proxy
+{
+	//See: https://github.com/Sylverant/libsylverant/blob/7f7e31d90da1b02c8d89d055628540ee3ad59417/include/sylverant/characters.h#L167
+	//Based on Sylverant's sylverant_bb_key_team_config_t
+	/// <summary>
+	/// Payload that is similar to <see cref="CharacterOptionsResponsePayload"/> with options data.
+	/// Sent by the client in cases including when <see cref="CharacterOptionsResponsePayload"/> data is incorrect.
+	/// </summary>
+	[WireDataContract]
+	[GameClientPacketPayload(GameNetworkOperationCode.BB_OPTION_CONFIG_TYPE)]
+	public sealed partial class CharacterOptionsUpdateRequestPayload : PSOBBGamePacketPayloadClient
+	{
+		/// <summary>
+		/// Unknown bytes.
+		/// </summary>
+		[KnownSize(276)]
+		[WireMember(1)]
+		internal byte[] unk { get; set; }
+
+		/// <summary>
+		/// Binding configuration.
+		/// </summary>
+		[WireMember(2)]
+		public BindingsConfig Bindings { get; internal set; }
+
+		/// <summary>
+		/// The guild card for the account.
+		/// </summary>
+		[WireMember(3)]
+		public uint GuildCard { get; internal set; }
+
+		/// <summary>
+		/// The team information.
+		/// </summary>
+		[WireMember(4)]
+		public AccountTeamInformation TeamInfo { get; internal set; }
+
+		public CharacterOptionsUpdateRequestPayload(BindingsConfig bindings, uint guildCard, AccountTeamInformation teamInfo) 
+			: this()
+		{
+			unk = new byte[276];
+			Bindings = bindings;
+			GuildCard = guildCard;
+			TeamInfo = teamInfo;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		public CharacterOptionsUpdateRequestPayload()
+			: base(GameNetworkOperationCode.BB_OPTION_CONFIG_TYPE)
+		{
+			
+		}
+	}
+}
