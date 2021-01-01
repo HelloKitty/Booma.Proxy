250aa047c7e431e071e7e9ff2b2e01f2a30b6842
HelloKitty <andrew.blakely@ymail.com>
2021-01-01 02:30:20 -0600

    Added KnownMenuIdentifier

---
+++ src/Booma.API.Common/UI/KnownMenuIdentifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma.UI
+{
+	//See: https://github.com/Sylverant/ship_server/blob/83d06b342c2629fc6812ed49000163e95b1a5fb7/src/ship_packets.h#L61
+	/// <summary>
+	/// Enumeration of known menu identifiers.
+	/// </summary>
+	public enum KnownMenuIdentifier : uint
+	{
+		INFODESK = 0x00000000,
+		BLOCK = 0x00000001,
+		GAME = 0x00000002,
+		QCATEGORY = 0x00000003,
+		QUEST = 0x00000004,
+		SHIP = 0x00000005,
+		GAME_TYPE = 0x00000006,
+		GM = 0x00000007,
+		LOBBY = 0xFFFFFFFF
+	}
+}
