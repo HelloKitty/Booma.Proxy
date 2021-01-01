ee32e3347697fb279c664b79159f0380d6725826
HelloKitty <andrew.blakely@ymail.com>
2021-01-01 02:56:20 -0600

    Added CharacterStatType

---
+++ src/Booma.API.Common/Character/Enum/CharacterStatType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	/// <summary>
+	/// Enumeration of player stat types.
+	/// </summary>
+	public enum CharacterStatType
+	{
+		ATP = 0,
+		MST = 1,
+		EVP = 2,
+		HP = 3,
+		DFP = 4,
+		ATA = 5,
+		LCK = 6
+	}
+}
