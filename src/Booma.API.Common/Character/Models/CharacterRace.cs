205a4dec5f0f8580c564ebd6d5fcce4b7340164c
HelloKitty <andrew.blakely@ymail.com>
2021-02-22 00:14:17 -0600

    Added CharacterRace enum

---
+++ src/Booma.API.Common/Character/Models/CharacterRace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	//TODO: We need the actual values from the game somehow!
+	/// <summary>
+	/// Enumeration of PSOBB races.
+	/// </summary>
+	public enum CharacterRace
+	{
+		Human = 1,
+		Newman = 2,
+		Cast = 3,
+	}
+}
