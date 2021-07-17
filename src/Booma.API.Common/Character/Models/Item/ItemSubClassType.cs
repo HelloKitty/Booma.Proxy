6d9e7e73ce89c3f7817123d88a54ca8288b730a2
HelloKitty <andrew.blakely@ymail.com>
2021-07-17 00:01:20 -0500

    Added Item subclass enums

---
+++ src/Booma.API.Common/Character/Models/Item/ItemSubClassType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	public enum FrameItemSubClassType : byte
+	{
+		Frame = 1,
+		Barrier = 2,
+		Unit = 3
+	}
+
+	//Currently parsed from Teth only
+	public enum WeaponItemSubClassType : byte
+	{
+		Saber = 0x1,
+		Handgun = 0x06,
+		Cane = 0x0A
+	}
+}
