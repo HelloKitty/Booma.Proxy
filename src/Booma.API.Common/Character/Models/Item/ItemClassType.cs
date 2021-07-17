019da05dd11d7b422d275c04b86c4bb19670449c
HelloKitty <andrew.blakely@ymail.com>
2021-07-17 00:01:08 -0500

    Added ItemClassType

---
+++ src/Booma.API.Common/Character/Models/Item/ItemClassType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	/// <summary>
+	/// Based on the concept of WoW DBC's ItemClass.dbc 
+	/// </summary>
+	public enum ItemClassType : byte
+	{
+		Weapon = 0,
+		Guard = 1,
+		Mag = 2,
+		Tool = 3,
+		Meseta = 4
+	}
+}
