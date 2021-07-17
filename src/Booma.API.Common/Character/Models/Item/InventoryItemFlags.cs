1b045809224f09e3ea320c68280e9cc10f58a2ba
HelloKitty <andrew.blakely@ymail.com>
2021-07-17 00:01:00 -0500

    Added InventoryItemFlags

---
+++ src/Booma.API.Common/Character/Models/Item/InventoryItemFlags.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	[Flags]
+	public enum InventoryItemFlags : uint
+	{
+		None = 0,
+
+		//I think this is banked lol
+		/// <summary>
+		/// Indicates that the item is in the bank (maybe?).
+		/// </summary>
+		Banked = 1 << 0,
+
+		/// <summary>
+		/// Flag indicates that the item is equipped.
+		/// </summary>
+		Equipped = 1 << 3,
+	}
+}
